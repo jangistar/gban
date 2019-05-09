7f54f0e094b047d5553bd74e94ef0a662827fe40
Ravana <47528708+ravana69@users.noreply.github.com>
2019-05-09 22:54:46 +0530

    Update _help.py

--- stdplugins/_help.py
+++ stdplugins/_help.py
@@ -1,17 +1,20 @@
-import sys
-from telethon import events, functions, __version__
+import sys 
+import psutil 
+import cpuinfo
+from telethon import events, functions, __version__ 
+from telethon.utils import get_input_location
+from datetime import datetime, timedelta
 from uniborg.util import admin_cmd
-
 
 @borg.on(admin_cmd(pattern="helpme", allow_sudo=True))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
-    help_string = """@UniBorg
+    help_string = """@r4v4n4
 Python {}
 Telethon {}
 
-UserBot Forked from https://github.com/expectocode/uniborg""".format(
+UserBot Forked from https://github.com/ravana69/uniborg""".format(
         sys.version,
         __version__
     )
@@ -48,3 +51,34 @@ async def _(event):
     result = result.stringify()
     logger.info(result)  # pylint:disable=E0602
     await event.edit("""Telethon UserBot powered by @UniBorg""")
+
+@borg.on(admin_cmd("server")) 
+async def _(event):
+    if event.fwd_from:
+        return 
+    start = datetime.now()
+    await event.edit("```Collecting UserBot Stats...```")
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    with open('/proc/uptime', 'r') as f: 
+        uptime_seconds = float(f.readline().split()[0]) 
+        uptime_string = str(timedelta(seconds = uptime_seconds))
+        cpu = cpuinfo.get_cpu_info()['brand'] #psutil.cpu_freq(percpu=False)
+        d = psutil.disk_usage('/')
+    start_string = """
+    🔥UserBot Stats🔥
+
+    __Owner__ : [r4v4n4](https://t.me/r4v4n4)
+    ```Status :``` Online
+PING:  ```{}```ms
+```Dc : 5 IE``` 
+```Python : {}
+Telethon : {}``` 
+```Plugins :``` {}
+```Uptime :``` {} 
+```Cpuinfo :``` {}
+```Disk_usage :``` {}/100
+[⠀](https://telegra.ph/file/623750150feb044d80199.mp4)""".format(ms,
+        sys.version,
+        __version__,len(borg._plugins),uptime_string,cpu,d.percent)
+    await event.edit(start_string,link_preview=True)
