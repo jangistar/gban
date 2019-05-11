b8262902410c81d78a848d950640b6f7cda8a61c
Ravana <47528708+ravana69@users.noreply.github.com>
2019-05-12 01:00:52 +0530

    Update _help.py

--- stdplugins/_help.py
+++ stdplugins/_help.py
@@ -6,10 +6,15 @@ from telethon.utils import get_input_location
 from datetime import datetime, timedelta
 from uniborg.util import admin_cmd
 
-@borg.on(admin_cmd(pattern="helpme", allow_sudo=True))  # pylint:disable=E0602
+@borg.on(admin_cmd(pattern="helpme ?(.*)", allow_sudo=True))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
+splugin_name = event.pattern_match.group(1)
+    if splugin_name in borg._plugins:
+        s_help_string = borg._plugins[splugin_name].__doc__
+    else:
+        s_help_string = "Module Not Loaded"
     help_string = """@r4v4n4
 Python {}
 Telethon {}
@@ -31,7 +36,7 @@ UserBot Forked from https://github.com/ravana69/uniborg""".format(
         )
         await event.delete()
     else:
-        await event.reply(help_string)
+        await event.reply(help_string + "\n\n" + s_help_string)
         await event.delete()
 
 
