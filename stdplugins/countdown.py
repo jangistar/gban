3b885e56c582b49a87d869cd8e2a59a59722c1be
Ravana <47528708+ravana69@users.noreply.github.com>
2019-05-15 10:57:45 +0530

    Update countdown.py

--- stdplugins/countdown.py
+++ stdplugins/countdown.py
@@ -76,3 +76,26 @@ async def timer_blankx(e):
  else:
 
   await e.edit(txt + 'NaN')
+
+
+@borg.on(events.NewMessage(outgoing=True, pattern='^\.(f?f)admin '))
+
+async def timer_blankx(e):
+
+ txt=e.text[7:] + '\n\n`Promoting you as an admin in` '
+
+ j=5
+
+ k=j
+
+ for j in range(j):
+
+  await e.edit(txt + str(k))
+
+  k=k-1
+
+  await asyncio.sleep(1)
+
+ if e.pattern_match.group(1) == 'f':
+
+  await e.edit("`Congratulation, User promoted as admin.` ")
