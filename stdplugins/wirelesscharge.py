813e49d450f1648c9dcdb24fd888e072d8a4416f
Ravana <47528708+ravana69@users.noreply.github.com>
2019-05-26 12:39:22 +0530

    Create wirelesscharge.py

---
+++ stdplugins/wirelesscharge.py
@@ -0,0 +1,50 @@
+# For @UniBorg
+
+"""Countdown Commands
+
+.timer
+
+.fcd"""
+
+
+
+from telethon import events
+
+from datetime import datetime
+
+from uniborg.util import admin_cmd
+
+import importlib.util
+
+import asyncio
+
+import random
+
+import importlib.util
+
+
+
+
+@borg.on(events.NewMessage(outgoing=True, pattern='^\.(f?f)charge '))
+
+async def timer_blankx(e):
+
+ txt=e.text[7:] + '\n\n`Wireless Charging (beta) Starts In` '
+
+ j=5
+
+ k=j
+
+ for j in range(j):
+
+  await e.edit(txt + str(k))
+
+  k=k-1
+
+  await asyncio.sleep(1)
+
+ if e.pattern_match.group(1) == 'c':
+
+  await e.edit("`Battery Charging... Battery Percentage : 59%` ")
+
+
