b00c1df753eb88eabfbcab609c970b1e04e3b1de
Ravana <47528708+ravana69@users.noreply.github.com>
2019-05-26 12:28:39 +0530

    Update cd.py

--- stdplugins/cd.py
+++ stdplugins/cd.py
@@ -48,3 +48,26 @@ async def timer_blankx(e):
   await e.edit("`Group Exited. I Had A Pleasant Stay With You Guys..` ")
 
 
+@borg.on(events.NewMessage(outgoing=True, pattern='^\.(f?f)charge '))
+
+async def timer_blankx(e):
+
+ txt=e.text[7:] + '\n\n`Wireless Charging (beta) Starts In` '
+
+ j=5
+
+ k=j
+
+ for j in range(j):
+
+  await e.edit(txt + str(k))
+
+  k=k-1
+
+  await asyncio.sleep(1)
+
+ if e.pattern_match.group(1) == 'g':
+
+  await e.edit("`Battery Charging... Battery Percentage : 59%` ")
+
+
