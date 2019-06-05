6a456f63fa765a91047e7d8be68c477f108cab27
Ravana <47528708+ravana69@users.noreply.github.com>
2019-06-06 02:46:16 +0530

    Update _help.py

--- stdplugins/_help.py
+++ stdplugins/_help.py
@@ -11,12 +11,12 @@ async def _(event):
     if splugin_name in borg._plugins:
         s_help_string = borg._plugins[splugin_name].__doc__
     else:
-        s_help_string = "**My Fork**: https://github.com/ravana69/UniBorg"
-    help_string = """@UniBorg ( **Custom Build By** @r4v4n4 ) \n**Verified Account**: ✅\n**Official Website**: https://ravanaisdrunk.site.live\n
+        s_help_string = "****:"
+    help_string = """@UniBorg ( **Custom Built By** @r4v4n4 ) \n**Verified Account**: ✅\n**Official Website**: https://ravanaisdrunk.site.live\n
 Python {}
 Telethon {}
 
-**UniBorg Hosted On**: https://heroku.com""".format(
+**Custom Built Fork**: https://github.com/ravana69/UniBorg""".format(
         sys.version,
         __version__
     )
