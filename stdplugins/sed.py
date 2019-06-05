21ef9aa8f6b039d6daf809994962889766cde6be
Ravana <47528708+ravana69@users.noreply.github.com>
2019-06-06 01:58:05 +0530

    Update sed.py

--- stdplugins/sed.py
+++ stdplugins/sed.py
@@ -5,7 +5,7 @@ import regex
 from telethon import events, utils
 from telethon.tl import types, functions
 
-HEADER = "「👇 Edited 👇」\n"
+HEADER = "👇 Edited 👇\n"
 KNOWN_RE_BOTS = re.compile(
     r'(regex|moku|BananaButler_|rgx|l4mR|kochu)bot',
     flags=re.IGNORECASE
