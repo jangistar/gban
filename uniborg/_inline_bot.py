3f214e325f2f82fc21d22fee944b5ee15647ac99
Ravana <47528708+ravana69@users.noreply.github.com>
2019-06-06 21:29:25 +0530

    edot

--- uniborg/_inline_bot.py
+++ uniborg/_inline_bot.py
@@ -262,7 +262,7 @@ def paginate_help(page_number, loaded_plugins, prefix):
             helpable_plugins.append(p)
     helpable_plugins = sorted(helpable_plugins)
     modules = [custom.Button.inline(
-        "{} {}".format("👮‍♂️Telegram Officer👮‍♂️⠀⠀", x),
+        "{} {}".format("👮‍♂️", x),
         data="ub_plugin_{}".format(x))
         for x in helpable_plugins]
     pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
@@ -273,8 +273,8 @@ def paginate_help(page_number, loaded_plugins, prefix):
     if len(pairs) > number_of_rows:
         pairs = pairs[modulo_page * number_of_rows:number_of_rows * (modulo_page + 1)] + \
             [
-            (custom.Button.inline("⠀", data="{}_prev({})".format(prefix, modulo_page)),
-             custom.Button.inline("⠀", data="{}_next({})".format(prefix, modulo_page)))
+            (custom.Button.inline("⏪", data="{}_prev({})".format(prefix, modulo_page)),
+             custom.Button.inline("⏩", data="{}_next({})".format(prefix, modulo_page)))
         ]
     return pairs
 
