db55b5107763e98a8ae9915dd6a4a72ac395c2ef
Ravana <47528708+ravana69@users.noreply.github.com>
2019-06-09 18:40:55 +0530

    Update load.py

--- stdplugins/load.py
+++ stdplugins/load.py
@@ -188,3 +188,42 @@ async def _(event):
             await asyncio.sleep(animation_interval)
 
             await event.edit(animation_chars[i % 4])
+
+
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.1
+
+    animation_ttl = range(0, 100)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "heart":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "🖤",
+
+            "❤️",
+
+            "🖤",
+
+            "❤️"
+            "‎"
+
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 4])
