3b3135f42908debe9a9c1521d4c510f6fa28138d
Ravana <47528708+ravana69@users.noreply.github.com>
2019-06-09 14:33:30 +0530

    Update warnbun.py

--- stdplugins/warnbun.py
+++ stdplugins/warnbun.py
@@ -67,3 +67,48 @@ async def _(event):
         await event.reply(mentions)
     await event.delete()
 
+""".admin Plugin for @UniBorg"""
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from uniborg.util import admin_cmd
+
+
+@borg.on(admin_cmd("warn0"))
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = "`Warning Resetted By Admin...\nYou Have  0/3  warnings`"
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f"[\u2063](tg://user?id={x.id})"
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete()
+
+""".admin Plugin for @UniBorg"""
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from uniborg.util import admin_cmd
+
+
+@borg.on(admin_cmd("gbun"))
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = "`Warning!! User Gbanned By Admin...\nReason: Potential Spammer. `"
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f"[\u2063](tg://user?id={x.id})"
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete()
