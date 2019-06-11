85e5c78d2f94e9daaed503337291dce03a621365
Ravana <47528708+ravana69@users.noreply.github.com>
2019-06-11 21:33:17 +0530

    Update fuck.py

--- stdplugins/fuck.py
+++ stdplugins/fuck.py
@@ -55,6 +55,44 @@ async def _(event):
             await event.edit(animation_chars[i % 4])
 
 
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.2
+
+    animation_ttl = range(0, 101)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "sux":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "🤵       👰",
+
+            "🤵     👰",
+
+            "🤵  👰",
+
+            "🤵👼👰"
+
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 4])
+
+
+
 """Emoji
 
 Available Commands:
@@ -101,7 +139,7 @@ async def _(event):
 
             "🤵  👰",
 
-            "🤵👼👰"
+            "🤵💋👰"
 
         ]
 
