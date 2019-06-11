8764e706533cb51ed354bc3bc2471e29c936d05d
Ravana <47528708+ravana69@users.noreply.github.com>
2019-06-11 21:23:50 +0530

    Update fuck.py

--- stdplugins/fuck.py
+++ stdplugins/fuck.py
@@ -38,13 +38,70 @@ async def _(event):
 
         animation_chars = [
 
-            "8===========✊️0  ",
+            "👉       ✊️",
 
-            "8======✊️=====0  ",
+            "👉     ✊️",
 
-            "8===✊️========0  ",
+            "👉  ✊️",
 
-            "8=✊️==========0💦"
+            "👉✊️💦"
+
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 4])
+
+
+"""Emoji
+
+Available Commands:
+
+.emoji shrug
+
+.emoji apple
+
+.emoji :/
+
+.emoji -_-"""
+
+from telethon import events
+
+import asyncio
+
+
+
+
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.2
+
+    animation_ttl = range(0, 101)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "kess":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "👰       🤵",
+
+            "👰     🤵",
+
+            "👰  🤵",
+
+            "💏"
 
         ]
 
