a9c5f9cf757f2169a484795a282541e955a2817e
Ravana <47528708+ravana69@users.noreply.github.com>
2019-06-11 16:59:21 +0530

    Create fuck.py

---
+++ stdplugins/fuck.py
@@ -0,0 +1,56 @@
+"""Emoji
+
+Available Commands:
+
+.emoji shrug
+
+.emoji apple
+
+.emoji :/
+
+.emoji -_-"""
+
+from telethon import events
+
+import asyncio
+
+
+
+
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.1
+
+    animation_ttl = range(0, 100)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "fu":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "🤚",
+
+            "🖕",
+
+            "🤚",
+
+            "🖕"
+            "‎🤚"
+
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 4])
