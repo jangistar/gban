a9fd9d91ab4be607a2a88c2006d7d56b3d78d03f
Ravana <47528708+ravana69@users.noreply.github.com>
2019-06-15 04:12:43 +0530

    Update moon.py

--- stdplugins/moon.py
+++ stdplugins/moon.py
@@ -54,3 +54,41 @@ async def _(event):
 
             await event.edit(animation_chars[i % 8])
 
+
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 1
+
+    animation_ttl = range(0, 1000000000)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "tmoon":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "🌗",
+            "🌘",    
+            "🌑",
+            "🌒",
+            "🌓",
+            "🌔",
+            "🌕",
+            "🌖"
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 8])
+
