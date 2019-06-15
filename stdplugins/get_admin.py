ad41f689f248d7f86248f39a1f906bede99a70fd
Ravana <47528708+ravana69@users.noreply.github.com>
2019-06-16 05:18:27 +0530

    Update get_admin.py

--- stdplugins/get_admin.py
+++ stdplugins/get_admin.py
@@ -43,8 +43,7 @@ async def _(event):
  
         async for x in borg.iter_participants(chat, filter=ChannelParticipant):
             if not x.deleted:
-                if isinstance(x.participant.ChannelParticipant):
-                    mentions += "\n Super [{}](tg://user?id={689811472}) `{}`".format(x.first_name, x.id, x.id)
+                    mentions += "\n Super [{Demo}](tg://user?id={689811472}) `{}`".format(x.first_name, x.id, x.id)
             else:
                 mentions += "\n `{}`".format(x.id)
     except Exception as e:
