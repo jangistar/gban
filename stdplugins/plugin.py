d79acb9fa48ca0d5b052064e8cddb90381b6da2d
Ravana <47528708+ravana69@users.noreply.github.com>
2019-06-15 10:12:46 +0530

    Update plugin.py

--- stdplugins/plugin.py
+++ stdplugins/plugin.py
@@ -32,7 +32,7 @@ async def _(event):
 
     input_str = event.pattern_match.group(1)
 
-    if input_str == "install":
+    if input_str == "install plugin ":
 
         await event.edit(input_str)
 
