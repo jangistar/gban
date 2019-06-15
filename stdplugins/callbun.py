bfef34498a9fe6555003b6dd55ffde8247d1f64b
Ravana <47528708+ravana69@users.noreply.github.com>
2019-06-15 08:12:58 +0530

    Update callbun.py

--- stdplugins/callbun.py
+++ stdplugins/callbun.py
@@ -18,7 +18,7 @@ import asyncio
 
 
 
-@borg.on(events.NewMessage(pattern=r"\.(*)", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 
 async def _(event):
 
@@ -39,7 +39,7 @@ async def _(event):
         animation_chars = [
         
             "`Connecting To Telegram Headquarters...`",
-            "`User Authorised.**",
+            "`User Authorised.`",
             "`Private VOIP Call Connected...`",
             "`Me Calling Pavel Durov Shukla....`",
             "`Me: Hello Sir, Please Ban This Guys Telegram Account.`",    
