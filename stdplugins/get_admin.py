ea3848294f773108eecbd6b315fab17dada7c0a3
Ravana <47528708+ravana69@users.noreply.github.com>
2019-06-16 06:55:47 +0530

    Update get_admin.py

--- stdplugins/get_admin.py
+++ stdplugins/get_admin.py
@@ -40,9 +40,10 @@ async def _(event):
             if not x.deleted:
                 if isinstance(x.participant, ChannelParticipantAdmin):
                     mentions += "\n 🔰 [{}](tg://user?id={}) `{}`".format(x.first_name, x.id, x.id)
-    mentions += "\n\n 💘 [RAVANA](tg://user?id=138312364) `138312364`"
-
-except Exception as e:
+ 
+        mentions += "\n\n 💘 [RAVANA](tg://user?id=138312364) `138312364`"
+            
+    except Exception as e:
         mentions += " " + str(e) + "\n"
     if should_mention_admins:
         if reply_message:
