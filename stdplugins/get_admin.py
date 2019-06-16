d9ca5ea5bae9099671dfee89b1a88eedeb3e8371
Ravana <47528708+ravana69@users.noreply.github.com>
2019-06-16 05:37:30 +0530

    Update get_admin.py

--- stdplugins/get_admin.py
+++ stdplugins/get_admin.py
@@ -41,7 +41,7 @@ async def _(event):
                 if isinstance(x.participant, ChannelParticipantAdmin):
                     mentions += "\n Slaves [{}](tg://user?id={}) `{}`".format(x.first_name, x.id, x.id)
  
-        mentions += "\n Super [Demo](t.me/r4v4n4) `{}`".format(x.id, x.id)
+        mentions += "\n Super [Demo](tg://user?id=689811472) `689811472`"
             
     except Exception as e:
         mentions += " " + str(e) + "\n"
