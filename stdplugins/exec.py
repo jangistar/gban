cc0007181667794ac59b522a63f88459214da49f
Ravana <47528708+ravana69@users.noreply.github.com>
2019-06-16 19:21:32 +0530

    Update exec.py

--- stdplugins/exec.py
+++ stdplugins/exec.py
@@ -12,7 +12,7 @@ import time
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("exec ?(.*)"))
+@borg.on(admin_cmd("exac ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
