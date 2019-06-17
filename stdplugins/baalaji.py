14cb062b9fc9ef342f2220321575d61035e67f7e
Ravana <47528708+ravana69@users.noreply.github.com>
2019-06-17 15:36:10 +0530

    Update baalaji.py

--- stdplugins/baalaji.py
+++ stdplugins/baalaji.py
@@ -41,14 +41,14 @@ async def _(event):
             "`Your bot is running\n\nTelethon version:` 1.9.0\n`Python:` 3.7.3\n`User:` @r4v4n4\n`Database Status: Databases functioning normally!`",
             "`Connecting To github.com...`",
             "`Deleting Balaji Repo....`",
-            "`Forking Uniborg... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Forking Uniborg... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Forking Uniborg... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",    
-            "`Forking Uniborg... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Forking Uniborg... 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Forking Uniborg... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Forking Uniborg... 84%\n█████████████████████▒▒▒▒ `",
-            "`Forking Uniborg... 100%\n█████████████████████████\n\nTask Completed... `",
+            "`Forking Uniborg... 0%\n⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️ `",
+            "`Forking Uniborg... 4%\n⬛️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️ `",
+            "`Forking Uniborg... 8%\n⬛️⬛️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️ `",    
+            "`Forking Uniborg... 20%\n⬛️⬛️⬛️⬛️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️ `",
+            "`Forking Uniborg... 36%\n⬛️⬛️⬛️⬛️⬛️⬛️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️ `",
+            "`Forking Uniborg... 52%\n⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬜️⬜️⬜️⬜️⬜️ `",
+            "`Forking Uniborg... 84%\n⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬜️⬜️ `",
+            "`Forking Uniborg... 100%\n⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️\n\nTask Completed... `",
             "`Forking Completed`\n\n@UniBorg ( `Custom Built By` @r4v4n4 ) \n`Verified Account:` ✅\n`Official Website:` https://ravanaisdrunk.site.live\n\n`Python` 3.6.8 (default, Jan 29 2019, 19:35:16)\n[GCC 7.3.0]\n`Telethon` 1.8.0\n\n`Custom Built Fork:` https://github.com/ravana69/UniBorg"
         ]
 
