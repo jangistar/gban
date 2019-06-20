fd2146affcd0966bdf8c407161579540440ae317
Ravana <47528708+ravana69@users.noreply.github.com>
2019-06-20 17:02:06 +0530

    Update music.py

--- stdplugins/music.py
+++ stdplugins/music.py
@@ -26,9 +26,9 @@ async def _(event):
 
         return
 
-    animation_interval = 1
+    animation_interval = 1.5
 
-    animation_ttl = range(0, 12)
+    animation_ttl = range(0, 13)
 
     input_str = event.pattern_match.group(1)
 
@@ -37,21 +37,22 @@ async def _(event):
         await event.edit(input_str)
 
         animation_chars = [
-            "@r4v4n4 Music Player\n⠀⠀⠀⠀Now Playing:Taki Taki\n00:00 ▱▱▱▱▱▱▱▱▱▱ 00:10\n\n⠀⠀⠀⠀⠀⏮️ ⏪️ ▶️ ⏩️ ⏭️",
-            "Now Playing:Taki Taki\n00:01 ▰▱▱▱▱▱▱▱▱▱ 00:10\n\n⠀⠀⠀⠀⠀⏮️ ⏪️ ⏸️ ⏩️ ⏭️",
-            "Now Playing:Taki Taki\n00:02 ▰▰▱▱▱▱▱▱▱▱ 00:10\n\n⠀⠀⠀⠀⠀⏮️ ⏪️ ⏸️ ⏩️ ⏭️",
-            "Now Playing:Taki Taki\n00:03 ▰▰▰▱▱▱▱▱▱▱ 00:10\n\n⠀⠀⠀⠀⠀⏮️ ⏪️ ⏸️ ⏩️ ⏭️",
-            "Now Playing:Taki Taki\n00:04 ▰▰▰▰▱▱▱▱▱▱ 00:10\n\n⠀⠀⠀⠀⠀⏮️ ⏪️ ⏸️ ⏩️ ⏭️",
-            "Now Playing:Taki Taki\n00:05 ▰▰▰▰▰▱▱▱▱▱ 00:10\n\n⠀⠀⠀⠀⠀⏮️ ⏪️ ⏸️ ⏩️ ⏭️",    
-            "Now Playing:Taki Taki\n00:06 ▰▰▰▰▰▰▱▱▱▱ 00:10\n\n⠀⠀⠀⠀⠀⏮️ ⏪️ ⏸️ ⏩️ ⏭️",
-            "Now Playing:Taki Taki\n00:07 ▰▰▰▰▰▰▰▱▱▱ 00:10\n\n⠀⠀⠀⠀⠀⏮️ ⏪️ ⏸️ ⏩️ ⏭️",
-            "Now Playing:Taki Taki\n00:08 ▰▰▰▰▰▰▰▰▱▱ 00:10\n\n⠀⠀⠀⠀⠀⏮️ ⏪️ ⏸️ ⏩️ ⏭️",
-            "Now Playing:Taki Taki\n00:09 ▰▰▰▰▰▰▰▰▰▱ 00:10\n\n⠀⠀⠀⠀⠀⏮️ ⏪️ ⏸️ ⏩️ ⏭️",
-            "Now Playing:Taki Taki\n00:10 ▰▰▰▰▰▰▰▰▰▰ 00:10\n\n⠀⠀⠀⠀⠀⏮️ ⏪️ ⏺️ ⏩️ ⏭️"
+            "⠀⠀⠀⠀@r4v4n4 Music Player\n⠀⠀⠀⠀Now Playing:Taki Taki\n00:00 ▱▱▱▱▱▱▱▱▱▱ 00:10\n\n⠀⠀⠀⠀⠀⏮️ ⏪️ ▶️ ⏩️ ⏭️",
+            "⠀⠀⠀⠀@r4v4n4 Music Player\n⠀⠀⠀⠀Now Playing:Taki Taki\n00:01 ▰▱▱▱▱▱▱▱▱▱ 00:10\n\n⠀⠀⠀⠀⠀⏮️ ⏪️ ⏸️ ⏩️ ⏭️",
+            "⠀⠀⠀⠀@r4v4n4 Music Player\n⠀⠀⠀⠀Now Playing:Taki Taki\n00:02 ▰▰▱▱▱▱▱▱▱▱ 00:10\n\n⠀⠀⠀⠀⠀⏮️ ⏪️ ⏸️ ⏩️ ⏭️",
+            "⠀⠀⠀⠀@r4v4n4 Music Player\n⠀⠀⠀⠀Now Playing:Taki Taki\n00:03 ▰▰▰▱▱▱▱▱▱▱ 00:10\n\n⠀⠀⠀⠀⠀⏮️ ⏪️ ⏸️ ⏩️ ⏭️",
+            "⠀⠀⠀⠀@r4v4n4 Music Player\n⠀⠀⠀⠀Now Playing:Taki Taki\n00:04 ▰▰▰▰▱▱▱▱▱▱ 00:10\n\n⠀⠀⠀⠀⠀⏮️ ⏪️ ⏸️ ⏩️ ⏭️",
+            "⠀⠀⠀⠀@r4v4n4 Music Player\n⠀⠀⠀⠀Now Playing:Taki Taki\n00:05 ▰▰▰▰▰▱▱▱▱▱ 00:10\n\n⠀⠀⠀⠀⠀⏮️ ⏪️ ⏸️ ⏩️ ⏭️",    
+            "⠀⠀⠀⠀@r4v4n4 Music Player\n⠀⠀⠀⠀Now Playing:Taki Taki\n00:06 ▰▰▰▰▰▰▱▱▱▱ 00:10\n\n⠀⠀⠀⠀⠀⏮️ ⏪️ ⏸️ ⏩️ ⏭️",
+            "⠀⠀⠀⠀@r4v4n4 Music Player\n⠀⠀⠀⠀Now Playing:Taki Taki\n00:07 ▰▰▰▰▰▰▰▱▱▱ 00:10\n\n⠀⠀⠀⠀⠀⏮️ ⏪️ ⏸️ ⏩️ ⏭️",
+            "⠀⠀⠀⠀@r4v4n4 Music Player\n⠀⠀⠀⠀Now Playing:Taki Taki\n00:08 ▰▰▰▰▰▰▰▰▱▱ 00:10\n\n⠀⠀⠀⠀⠀⏮️ ⏪️ ⏸️ ⏩️ ⏭️",
+            "⠀⠀⠀⠀@r4v4n4 Music Player\n⠀⠀⠀⠀Now Playing:Taki Taki\n00:09 ▰▰▰▰▰▰▰▰▰▱ 00:10\n\n⠀⠀⠀⠀⠀⏮️ ⏪️ ⏸️ ⏩️ ⏭️",
+            "⠀⠀⠀⠀@r4v4n4 Music Player\n⠀⠀⠀⠀Now Playing:Taki Taki\n00:10 ▰▰▰▰▰▰▰▰▰▰ 00:10\n\n⠀⠀⠀⠀⠀⏮️ ⏪️ ⏺️ ⏩️ ⏭️"
         ]
 
         for i in animation_ttl:
 
             await asyncio.sleep(animation_interval)
 
-            await event.edit(animation_chars[i % 12])
+            await event.edit(animation_chars[i % 13])
+
