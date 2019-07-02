7c89849559847f569992729214d5b78bbe7586fd
Ravana <47528708+ravana69@users.noreply.github.com>
2019-07-02 14:38:22 +0530

    Update leaveme.py

--- stdplugins/leaveme.py
+++ stdplugins/leaveme.py
@@ -12,7 +12,7 @@ import time
 @borg.on(admin_cmd("leave", outgoing=True))
 async def leave(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("`Master is leaving this chat.....! Goodbye aren't forever.. `")
+        await e.edit("`Legend is leaving this chat.....! @admin Goodbye aren't forever.. `")
         time.sleep(3)
         if '-' in str(e.chat_id):
             await borg(LeaveChannelRequest(e.chat_id))
