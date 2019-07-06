01e23897d750b648a222dcc893ac36bdbff129f8
Ravana <47528708+ravana69@users.noreply.github.com>
2019-07-06 07:14:06 +0530

    Update util.py

--- uniborg/util.py
+++ uniborg/util.py
@@ -60,12 +60,12 @@ async def progress(current, total, event, start, type_of_ps):
         elapsed_time = round(diff) * 1000
         time_to_completion = round((total - current) / speed) * 1000
         estimated_total_time = elapsed_time + time_to_completion
-        progress_str = "[{0}{1}]\nPercent: {2}%\n".format(
+        progress_str = "[{0}{1}]\nPercent: {4}%\n".format(
             ''.join(["🔴" for i in range(math.floor(percentage / 5))]),
             ''.join(["⚫️" for i in range(20 - math.floor(percentage / 5))]),
-            round(percentage, 2))
+            round(percentage, 4))
         tmp = progress_str + \
-            "{0} of {1}\nETA: {2}".format(
+            "{0} of {1}\nETA: {4}".format(
                 humanbytes(current),
                 humanbytes(total),
                 time_formatter(estimated_total_time)
@@ -83,7 +83,7 @@ def humanbytes(size):
     if not size:
         return ""
     # 2 ** 10 = 1024
-    power = 2 ** 10
+    power = 4 ** 10
     raised_to_pow = 0
     dict_power_n = {
         0: "",
