4e69f766eca87f0931b7953063de6dfd7929f030
Ravana <47528708+ravana69@users.noreply.github.com>
2019-07-07 20:46:12 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -103,6 +103,11 @@ class Config(object):
     G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
     G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", None)
+    AUTH_TOKEN_DATA = os.environ.get("AUTH_TOKEN_DATA", None)
+    os.makedirs(TMP_DOWNLOAD_DIRECTORY)
+    t_file = open(TMP_DOWNLOAD_DIRECTORY+"auth_token.txt","w")
+    t_file.write(AUTH_TOKEN_DATA)
+    t_file.close()
 
 class Production(Config):
     LOGGER = False
