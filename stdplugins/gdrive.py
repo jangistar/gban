2fd570f88a6664d6c6df7c24299c741cd435402c
Ravana <47528708+ravana69@users.noreply.github.com>
2019-07-07 20:43:15 +0530

    Update gdrive.py

--- stdplugins/gdrive.py
+++ stdplugins/gdrive.py
@@ -33,6 +33,7 @@ CLIENT_SECRET = Config.G_DRIVE_CLIENT_SECRET
 OAUTH_SCOPE = "https://www.googleapis.com/auth/drive.file"
 # Redirect URI for installed apps, can be left as is
 REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob"
+parent_id = Config.GDRIVE_FOLDER_ID
 
 
 @borg.on(admin_cmd(pattern="ugdrive ?(.*)", allow_sudo=True))
@@ -147,12 +148,15 @@ def upload_file(http, file_path, file_name, mime_type):
     # Create Google Drive service instance
     drive_service = build("drive", "v2", http=http)
     # File body description
+    
     media_body = MediaFileUpload(file_path, mimetype=mime_type, resumable=True)
     body = {
         "title": file_name,
         "description": "backup",
-        "mimeType": mime_type,
+        "mimeType": mime_type
     }
+    if parent_id:
+        body[ 'parents' ] = [{'id': parent_id}]
     # Permissions body description: anyone who has link can upload
     # Other permissions can be found at https://developers.google.com/drive/v2/reference/permissions
     permissions = {
@@ -169,3 +173,10 @@ def upload_file(http, file_path, file_name, mime_type):
     file = drive_service.files().get(fileId=file["id"]).execute()
     download_url = file.get("webContentLink")
     return download_url
+
+@borg.on(admin_cmd(pattern="gfolder ?(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    folder_link = "https://drive.google.com/drive/u/2/folders/"+parent_id    
+    await event.edit("`Here is Your Gdrive Folder link : `\n"+folder_link)    
