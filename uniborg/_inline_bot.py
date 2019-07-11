036761682e89405fccd30a284e5026d1fbee251d
Ravana <47528708+ravana69@users.noreply.github.com>
2019-07-12 00:55:28 +0530

    Update _inline_bot.py

--- uniborg/_inline_bot.py
+++ uniborg/_inline_bot.py
@@ -204,10 +204,10 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                         "🐶Resistance Dog🐶", "https://t.me/addstickers/ResistanceDog")],
                     [custom.Button.url("👨‍💻Source Code👨‍💻", "https://github.com/ravana69/Pornhub")], custom.Button.url(
                         "❕❗Deploy Me❗❕", "https://da.gd/Emcf")],
-                    [custom.Button.url("🔰Update Fork🔰", "tg://need_update_for_some_feature")], custom.Button.url(
-                        "✳️Fork Boost✳️", "tg://some_unsupported_feature")], 
+                    [custom.Button.url("🔰Update Fork🔰", "tg://need_update_for_some_feature"], custom.Button.url(
+                        "✳️Fork Boost✳️", "tg://some_unsupported_feature)"], 
                     [custom.Button.url(
-                        "♻️Refresh Heroku♻️", "tg://chutiya")]
+                        "♻️Refresh Heroku♻️", "tg://chutiya"]
                 ],
                 link_preview=False
             )
