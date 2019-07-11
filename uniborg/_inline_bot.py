69949c1d472d9a2ac5a46d91f2d2f17907d437e1
Ravana <47528708+ravana69@users.noreply.github.com>
2019-07-12 01:19:50 +0530

    Update _inline_bot.py

--- uniborg/_inline_bot.py
+++ uniborg/_inline_bot.py
@@ -206,7 +206,7 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                     [custom.Button.url("❕❗Deploy Me❗❕", "https://da.gd/Emcf")],
                     [custom.Button.url("🔰Update Fork🔰", "tg://need_update_for_some_feature"), custom.Button.url(
                         "✳️Fork Boost✳️", "tg://some_unsupported_feature"), custom.Button.url(
-                        "♻️Refresh Heroku♻️", "tg://chutiya")
+                        "♻️Refresh Heroku♻️", "tg://chutiya")]
                 ],
                 link_preview=False
             )
