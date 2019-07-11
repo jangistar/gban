5510e67a98b52f51f95ae5d7525aeb45bf8f0c49
Ravana <47528708+ravana69@users.noreply.github.com>
2019-07-11 12:59:17 +0530

    Update dagd.py

--- stdplugins/dagd.py
+++ stdplugins/dagd.py
@@ -101,7 +101,7 @@ async def _(event):
     else:
         await event.edit("i can't seem to find {} on the internet".format(input_str))
 
-@borg.on(admin_cmd("mystat (.*)"))
+@borg.on(admin_cmd("myhead (.*)"))
 async def _(event):
     if event.fwd_from:
         return
@@ -109,7 +109,19 @@ async def _(event):
     sample_url = "https://da.gd/headers/{}".format(input_str)
     response_api = requests.get(sample_url).text
     if response_api:
-        await event.edit("**Whois**\n{}\n{}".format(input_str, response_api))
+        await event.edit("**Header**\n{}\n{}".format(input_str, response_api))
+    else:
+        await event.edit("i can't seem to find {} on the internet".format(input_str))
+
+@borg.on(admin_cmd("myup (.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    sample_url = "https://da.gd/up/{}".format(input_str)
+    response_api = requests.get(sample_url).text
+    if response_api:
+        await event.edit("**Header**\n{}\n{}".format(input_str, response_api))
     else:
         await event.edit("i can't seem to find {} on the internet".format(input_str))
 
