66cb8a3e1023de604ae5adc4ebd5af31c6b1f03e
Ravana <47528708+ravana69@users.noreply.github.com>
2019-07-12 23:25:22 +0530

    Update execmod.py

--- stdplugins/execmod.py
+++ stdplugins/execmod.py
@@ -183,7 +183,7 @@ async def _(event):
     PROCESS_RUN_TIME = 100
 #    dirname = event.pattern_match.group(1)
 #    tempdir = "localdir"
-    cmd = "pip --upgrade pip"
+    cmd = "pip install --upgrade pip"
 #    if dirname == tempdir:
 	
     eply_to_id = event.message.id
@@ -213,6 +213,49 @@ async def _(event):
     await event.edit(f"{OUTPUT}`{stdout.decode()}`")
 #    else:
 #        await event.edit("Unknown Command")
+
+@borg.on(events.NewMessage(pattern=r"\.mkc", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    DELAY_BETWEEN_EDITS = 0.3
+    PROCESS_RUN_TIME = 100
+#    dirname = event.pattern_match.group(1)
+#    tempdir = "localdir"
+    cmd = "touch stdplugins/afk.py && touch stdplugins/telegraph.py"
+#    if dirname == tempdir:
+	
+    eply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    start_time = time.time() + PROCESS_RUN_TIME
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    OUTPUT = f"**[Ravana's](tg://need_update_for_some_feature/) PIP UPGRADE:**\n"
+    stdout, stderr = await process.communicate()
+    if len(stdout) > Config.MAX_MESSAGE_SIZE_LIMIT:
+        with io.BytesIO(str.encode(stdout)) as out_file:	
+            out_file.name = "exec.text"
+            await borg.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption=OUTPUT,
+                reply_to=reply_to_id
+            )
+            await event.delete()
+    if stderr.decode():
+        await event.edit(f"**{stderr.decode()}**")
+        return
+    await event.edit(f"{OUTPUT}`{stdout.decode()}`")
+#    else:
+#        await event.edit("Unknown Command")
+
+
+
+
 
 
 
