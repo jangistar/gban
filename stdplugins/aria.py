281ffc355be435db9dc5bd0a5abe32523fa90a0e
Ravana <47528708+ravana69@users.noreply.github.com>
2019-07-30 18:34:58 +0530

    Update aria.py

--- stdplugins/aria.py
+++ stdplugins/aria.py
@@ -71,6 +71,35 @@ async def torrent_download(event):
     await asyncio.sleep(5)
     await m.delete()
 
+@borg.on(events.NewMessage(pattern=r"\.url", outgoing=True))
+async def magnet_download(event):
+	if event.fwd_from:
+		return
+	var = event.text[5:]
+	print(var)	
+	uris = [var]
+
+	#Add URL Into Queue 
+	try:	
+		download = aria2.add_uris(uris, options=None, position=None)
+	except Exception as e:
+		await event.edit("`Error:\n`"+str(e))
+		return
+
+	gid = download.gid
+	complete = None
+	while complete != True:
+		file = aria2.get_download(gid)
+		complete = file.is_complete
+		try:
+			msg = "Downloading File: "+str(file.name) +"\nSpeed: "+ str(file.download_speed_string())+"\n"+"Progress: "+str(file.progress_string())+"\nStatus: "+str(file.status)+"\nETA:  "+str(file.eta_string())+"\n\n"	
+			await event.edit(msg)
+			await asyncio.sleep(10)
+		except Exception as e:
+			#print(str(e))
+			pass	
+			
+	await event.edit("File Downloaded Successfully:\n`"+file.name+"`")
 
 @borg.on(admin_cmd("remtor"))
 async def remove_all(event):
