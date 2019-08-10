0673be0be2764e5bca8e7c7bc6b63fb7290ede01
Ravana <47528708+ravana69@users.noreply.github.com>
2019-08-10 18:17:47 +0530

    Update _inline_bot.py

--- uniborg/_inline_bot.py
+++ uniborg/_inline_bot.py
@@ -4,6 +4,7 @@
 from math import ceil
 import asyncio
 import json
+import random
 import re
 from telethon import events, custom
 from uniborg.util import admin_cmd, humanbytes
@@ -263,18 +264,19 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
 
 def paginate_help(page_number, loaded_plugins, prefix):
     number_of_rows = Config.NO_OF_BUTTONS_DISPLAYED_IN_H_ME_CMD
-    number_of_cols = 7
+    number_of_cols = 2
+    multi = "😇🤠🤡😈👿👹👺💀☠👻👽👾🤖💩😺😸😹😻😼😽🙀😿😾🙈🙉🙊👦👧👨👩👴👵👶"
     helpable_plugins = []
     for p in loaded_plugins:
         if not p.startswith("_"):
             helpable_plugins.append(p)
     helpable_plugins = sorted(helpable_plugins)
     modules = [custom.Button.inline(
-        "{} {}".format("👮‍♂️", x),
+        "{} {} {}".format(random.choice(list(multi)), x, random.choice(list(multi))),
         data="ub_plugin_{}".format(x))
         for x in helpable_plugins]
     pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
-    if len(modules) % number_of_cols == 0:
+    if len(modules) % number_of_cols == 1:
         pairs.append((modules[-1],))
     max_num_pages = ceil(len(pairs) / number_of_rows)
     modulo_page = page_number % max_num_pages
