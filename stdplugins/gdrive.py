35110116c7204e183ca8496ee0eaa3966090e091
Ravana <47528708+ravana69@users.noreply.github.com>
2019-08-22 19:12:38 +0530

    Update gdrive.py

--- stdplugins/gdrive.py
+++ stdplugins/gdrive.py
@@ -183,3 +183,102 @@ async def _(event):
         return
     folder_link = "https://drive.google.com/drive/u/2/folders/"+parent_id    
     await event.edit("`Here is Your Gdrive Folder link : `\n"+folder_link)    
+
+
+
+
+@borg.on(admin_cmd(pattern="gdrivesp https?://drive\.google\.com/drive/u/\d/folders/([-\w]{25,})", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    mone = await event.reply("Processing ...")
+    input_str = event.pattern_match.group(1)
+    if input_str:
+        G_DRIVE_F_PARENT_ID = input_str
+        await mone.edit("Custom Folder ID set successfully. The next uploads will upload to {G_DRIVE_F_PARENT_ID} till `.gdriveclear`")
+        await event.delete()
+    else:
+        await mone.edit("Send `.gdrivesp https://drive.google.com/drive/u/X/folders/Y` to set the folder to upload new files to")
+
+
+@borg.on(admin_cmd(pattern="gdriveclear", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    mone = await event.reply("Processing ...")
+    G_DRIVE_F_PARENT_ID = None
+    await mone.edit("Custom Folder ID cleared successfully.")
+    await event.delete()
+
+
+# Get mime type and name of given file
+def file_ops(file_path):
+    mime_type = guess_type(file_path)[0]
+    mime_type = mime_type if mime_type else "text/plain"
+    file_name = file_path.split("/")[-1]
+    return file_name, mime_type
+
+
+async def create_token_file(token_file, event):
+    # Run through the OAuth flow and retrieve credentials
+    flow = OAuth2WebServerFlow(
+        CLIENT_ID,
+        CLIENT_SECRET,
+        OAUTH_SCOPE,
+        redirect_uri=REDIRECT_URI
+    )
+    authorize_url = flow.step1_get_authorize_url()
+    async with event.client.conversation(Config.PRIVATE_GROUP_BOT_API_ID) as conv:
+        await conv.send_message(f"Go to the following link in your browser: {authorize_url} and reply the code")
+        response = conv.wait_event(events.NewMessage(
+            outgoing=True,
+            chats=Config.PRIVATE_GROUP_BOT_API_ID
+        ))
+        response = await response
+        code = response.message.message.strip()
+        credentials = flow.step2_exchange(code)
+        storage = Storage(token_file)
+        storage.put(credentials)
+        return storage
+
+
+def authorize(token_file, storage):
+    # Get credentials
+    if storage is None:
+        storage = Storage(token_file)
+    credentials = storage.get()
+    # Create an httplib2.Http object and authorize it with our credentials
+    http = httplib2.Http()
+    credentials.refresh(http)
+    http = credentials.authorize(http)
+    return http
+
+
+def upload_file(http, file_path, file_name, mime_type):
+    # Create Google Drive service instance
+    drive_service = build("drive", "v2", http=http)
+    # File body description
+    media_body = MediaFileUpload(file_path, mimetype=mime_type, resumable=True)
+    body = {
+        "title": file_name,
+        "description": "backup",
+        "mimeType": mime_type,
+    }
+    if G_DRIVE_F_PARENT_ID is not None:
+        body["parents"] = [{"id": G_DRIVE_F_PARENT_ID}]
+    # Permissions body description: anyone who has link can upload
+    # Other permissions can be found at https://developers.google.com/drive/v2/reference/permissions
+    permissions = {
+        "role": "reader",
+        "type": "anyone",
+        "value": None,
+        "withLink": True
+    }
+    # Insert a file
+    file = drive_service.files().insert(body=body, media_body=media_body).execute()
+    # Insert new permissions
+    drive_service.permissions().insert(fileId=file["id"], body=permissions).execute()
+    # Define file instance and get url for download
+    file = drive_service.files().get(fileId=file["id"]).execute()
+    download_url = file.get("webContentLink")
+    return download_url
