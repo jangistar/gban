cd76e9dcb5725fcc8242317197e1c785418c1e84
Ravana <47528708+ravana69@users.noreply.github.com>
2019-08-22 14:47:41 +0530

    Update aria.py

--- stdplugins/aria.py
+++ stdplugins/aria.py
@@ -73,7 +73,7 @@ async def magnet_download(event):
 			complete = file.is_complete
 			try:
 				if not file.error_message:
-					msg = "Downloading File: `"+str(file.name) +"`\nSpeed: "+ str(file.download_speed_string())+"\nProgress: "+str(file.progress_string())+"\nTotal Size: "+str(file.total_size_string())+"\nStatus: "+str(file.status)+"\nETA:  "+str(file.eta_string())+"\n\n"
+					msg = "Downloading File: `"+str(file.name) +"`\nSpeed: "+ str(file.download_speed_string())+"\nProgress: "+str(file.progress_string())+"\nTotal Size: "+str(file.total_length_string())+"\nStatus: "+str(file.status)+"\nETA:  "+str(file.eta_string())+"\n\n"
 					await event.edit(msg)
 					await asyncio.sleep(15)
 				else:
@@ -127,7 +127,7 @@ async def torrent_download(event):
 			file = aria2.get_download(gid)
 			complete = file.is_complete
 			if not file.error_message:
-				msg = "Downloading File: `"+str(file.name) +"`\nSpeed: "+ str(file.download_speed_string())+"\nProgress: "+str(file.progress_string())+"\nTotal Size: "+str(file.total_size_string())+"\nStatus: "+str(file.status)+"\nETA:  "+str(file.eta_string())+"\n\n"
+				msg = "Downloading File: `"+str(file.name) +"`\nSpeed: "+ str(file.download_speed_string())+"\nProgress: "+str(file.progress_string())+"\nTotal Size: "+str(file.total_length_string())+"\nStatus: "+str(file.status)+"\nETA:  "+str(file.eta_string())+"\n\n"
 				await event.edit(msg)
 				await asyncio.sleep(15)
 			else:
