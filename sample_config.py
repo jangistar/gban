11307695452ff59c14d41b67f4f3a2fb186cb1fb
Ravana <47528708+ravana69@users.noreply.github.com>
2019-08-25 18:23:52 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -64,6 +64,7 @@ class Config(object):
     # because of their ephemeral file system
     NO_LOAD = [
         "fwd",
+        "chatbot",
         "gban"
     ]
     # Get your own API key from https://www.remove.bg/ or
