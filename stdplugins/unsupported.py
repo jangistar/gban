df7374478b811dc039a612c33a245cc089c56ef9
Ravana <47528708+ravana69@users.noreply.github.com>
2019-08-25 22:52:23 +0530

    Update unsupported.py

--- stdplugins/unsupported.py
+++ stdplugins/unsupported.py
@@ -5,23 +5,6 @@ from telethon.tl.types import ChannelParticipantsAdmins
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("z"))
-async def _(event):
-    if event.fwd_from:
-        return
-    mentions = "**Plugins To Load:**\n\n`.load afk`\n\n`.load telegraph `"
-    chat = await event.get_input_chat()
-    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
-        mentions += f""
-    reply_message = None
-    if event.reply_to_msg_id:
-        reply_message = await event.get_reply_message()
-        await reply_message.reply(mentions)
-    else:
-        await event.reply(mentions)
-    await event.delete()
-
-
 @borg.on(admin_cmd("repo"))
 async def _(event):
     if event.fwd_from:
