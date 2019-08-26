3f8463772102c53b15da026ef273f3ffd9c463af
Ravana <47528708+ravana69@users.noreply.github.com>
2019-08-26 16:24:13 +0530

    Update exec.py

--- stdplugins/exec.py
+++ stdplugins/exec.py
@@ -16,8 +16,8 @@ from uniborg.util import admin_cmd
 async def _(event):
     if event.fwd_from:
         return
-    DELAY_BETWEEN_EDITS = 1.0
-    PROCESS_RUN_TIME = 500
+    DELAY_BETWEEN_EDITS = 0.3
+    PROCESS_RUN_TIME = 100
     cmd = event.pattern_match.group(1)
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
