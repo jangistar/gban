15148285773be2415d474d0068eb3f7b7a73dddd
Ravana <47528708+ravana69@users.noreply.github.com>
2019-08-28 18:53:17 +0530

    Update dagd.py

--- stdplugins/dagd.py
+++ stdplugins/dagd.py
@@ -24,7 +24,7 @@ async def _(event):
         await event.edit("i can't seem to find {} on the internet".format(input_str))
 
 
-@borg.on(admin_cmd("url (.*)"))
+@borg.on(admin_cmd("link (.*)"))
 async def _(event):
     if event.fwd_from:
         return
