37705a816c335cf03e1bbda8ebb57a6331ec7772
Ravana <47528708+ravana69@users.noreply.github.com>
2019-09-05 12:14:46 +0530

    wiki

--- stdplugins/wikipedia.py
+++ stdplugins/wikipedia.py
@@ -2,6 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+""" command: .wiki """
+
 from telethon import events
 import wikipedia
 
