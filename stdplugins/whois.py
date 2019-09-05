e175f1262d734f0c1c2c444c7a1d66e83eb93ea1
Ravana <47528708+ravana69@users.noreply.github.com>
2019-09-05 12:14:06 +0530

    rendi

--- stdplugins/whois.py
+++ stdplugins/whois.py
@@ -1,5 +1,5 @@
 """Get Telegram Profile Picture and other information
-Syntax: .whois @username"""
+Syntax: .rendi @username"""
 
 import html
 from telethon.tl.functions.photos import GetUserPhotosRequest
