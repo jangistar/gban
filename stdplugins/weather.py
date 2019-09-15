aa136f44b5d3dd74e685fa2bb644b4c814cbb845
Ravana <47528708+ravana69@users.noreply.github.com>
2019-09-15 15:09:02 +0530

    Update and rename openweathermap.py to weather.py

--- stdplugins/weather.py
+++ stdplugins/weather.py
@@ -1,13 +1,15 @@
 """Get weather data using OpenWeatherMap
-Syntax: .weather <Location> """
- 
+Syntax: .weather <Location>
+.wttr <location> """
+
 import aiohttp
+import io
 import time
 from datetime import tzinfo, datetime
 from uniborg.util import admin_cmd
- 
- 
-@borg.on(admin_cmd("weather (.*)"))
+
+
+@borg.on(admin_cmd(pattern="weather (.*)"))
 async def _(event):
     if event.fwd_from:
         return
@@ -47,3 +49,21 @@ async def _(event):
         )
     else:
         await event.edit(response_api["message"])
+
+
+@borg.on(admin_cmd(pattern="wttr (.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    sample_url = "https://wttr.in/{}.png"
+    # logger.info(sample_url)
+    input_str = event.pattern_match.group(1)
+    async with aiohttp.ClientSession() as session:
+        response_api_zero = await session.get(sample_url.format(input_str))
+        # logger.info(response_api_zero)
+        response_api = await response_api_zero.read()
+        with io.BytesIO(response_api) as out_file:
+            await event.reply(
+                file=out_file
+            )
+    await event.edit(input_str)
