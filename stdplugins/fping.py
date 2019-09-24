91b2205ecc08c62493a4aacbc2243a3b38e12036
Ravana <47528708+ravana69@users.noreply.github.com>
2019-09-24 21:36:07 +0530

    Update and rename testing.py to fping.py

--- stdplugins/fping.py
+++ stdplugins/fping.py
@@ -2,7 +2,7 @@
 
 Available Commands:
 
-.testing"""
+.ping"""
 
 from telethon import events
 
@@ -26,7 +26,7 @@ async def _(event):
 
     input_str = event.pattern_match.group(1)
 
-    if input_str == "test":
+    if input_str == "ping":
 
         await event.edit(input_str)
 
