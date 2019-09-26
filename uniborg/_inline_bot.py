9cada4d6b5e5dd65b9bf04858d9c28ff2362fd26
Ravana <47528708+ravana69@users.noreply.github.com>
2019-09-26 22:46:33 +0530

    Update _inline_bot.py

--- uniborg/_inline_bot.py
+++ uniborg/_inline_bot.py
@@ -4,6 +4,7 @@
 from math import ceil
 import asyncio
 import json
+import random
 import re
 from telethon import events, custom
 from uniborg.util import admin_cmd, humanbytes
@@ -20,7 +21,7 @@ async def _(event):
     search_query = event.pattern_match.group(2)
     try:
         output_message = ""
-        bot_results = await event.client.inline_query(
+        bot_results = await event.client.inline_query(  # pylint:disable=E0602
             bot_username,
             search_query
         )
@@ -49,7 +50,7 @@ async def _(event):
     i_plus_oneth_result = event.pattern_match.group(2)
     search_query = event.pattern_match.group(3)
     try:
-        bot_results = await event.client.inline_query(
+        bot_results = await borg.inline_query(  # pylint:disable=E0602
             bot_username,
             search_query
         )
@@ -70,7 +71,7 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             buttons = paginate_help(0, borg._plugins, "helpme")
             result = builder.article(
                 "© @UniBorg",
-                text="{}\nCurrently Loaded Plugins: {}".format(
+                text="{}\nℂ𝕦𝕣𝕣𝕖𝕟𝕥𝕝𝕪 𝕃𝕠𝕒𝕕𝕖𝕕 ℙ𝕝𝕦𝕘𝕚𝕟𝕤: {}".format(
                     query, len(borg._plugins)),
                 buttons=buttons,
                 link_preview=False
@@ -85,15 +86,23 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         else:
             result = builder.article(
                 "© @UniBorg",
-                text="""Try @UniBorg
-You can log-in as Bot or User and do many cool things with your Telegram account.
+                text="""@r4v4n4 **( Custom Built By** @r4v4n4 **)** 
+**Verified Account:** ✅
+**Official Website:** https://ravanaisdrunk.site.live
 
-All instaructions to run @UniBorg in your PC has been explained in https://github.com/SpEcHiDe/UniBorg""",
+**Pithun 3.7.4 (default, Sep 12 2019, 01:19:52)** 
+**[GCC 7.4.0]**
+**Talethrun 1.10.4**
+
+**Custom Built Fork:** https://github.com/ravana69/Pornhub""",
                 buttons=[
-                    [custom.Button.url("Join the Channel", "https://telegram.dog/UniBorg"), custom.Button.url(
-                        "Join the Group", "tg://some_unsupported_feature")],
-                    [custom.Button.url(
-                        "Source Code", "tg://some_unsupported_feature")]
+                    [custom.Button.url("👤Contact Creator👤", "https://telegram.dog/r4v4n4"), custom.Button.url(
+                        "📼Ravana Audio Memes📼", "https://t.me/tgaudiomemes")],
+                    [custom.Button.url("👨‍💻Source Code👨‍💻", "https://github.com/ravana69/Pornhub"), custom.Button.url(
+                        "❕❗Deploy Me❗❕", "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2Fravana69%2FPornHub%2F&template=https%3A%2F%2Fgithub.com%2Fravana69%2FPornHub%2F")],
+                    [custom.Button.url("🔰Update Fork🔰", "tg://need_update_for_some_feature"), custom.Button.url(
+                        "✳️Fork Boost✳️", "tg://some_unsupported_feature"), custom.Button.url(
+                        "♻️Refresh Heroku♻️", "tg://chutiya")]
                 ],
                 link_preview=False
             )
@@ -112,9 +121,8 @@ All instaructions to run @UniBorg in your PC has been explained in https://githu
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Please get your own @UniBorg, and don't edit my messages!"
+            reply_pop_up_alert = "⚠️ Warning: Don't Press Any Buttons ⚠️\n\nCustom Fork: https://github.com/ravana69/Pornhub\n\n\nNote: Bas kar BetiChod, Maa Ke Laude, Madarchod"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-
 
     @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
         data=re.compile(b"helpme_prev\((.+?)\)")
@@ -145,20 +153,21 @@ All instaructions to run @UniBorg in your PC has been explained in https://githu
         reply_pop_up_alert = help_string if help_string is not None else \
             "No DOCSTRING has been setup for {} plugin".format(plugin_name)
         reply_pop_up_alert += "\n\n Use .unload {} to remove this plugin\n\
-            © @UniBorg".format(plugin_name)
+            © @r4v4n4".format(plugin_name)
         await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
 
 def paginate_help(page_number, loaded_plugins, prefix):
     number_of_rows = Config.NO_OF_BUTTONS_DISPLAYED_IN_H_ME_CMD
     number_of_cols = 2
+    multi = "😇🤠🤡😈👿👹👺💀☠👻👽👾🤖💩😺😸😹😻😼😽🙀😿😾🙈🙉🙊👦👧👨👩👴👵👶"
     helpable_plugins = []
     for p in loaded_plugins:
         if not p.startswith("_"):
             helpable_plugins.append(p)
     helpable_plugins = sorted(helpable_plugins)
     modules = [custom.Button.inline(
-        "{} {}".format("✅", x),
+        "{} {} {}".format(random.choice(list(multi)), x, random.choice(list(multi))),
         data="ub_plugin_{}".format(x))
         for x in helpable_plugins]
     pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
@@ -169,7 +178,7 @@ def paginate_help(page_number, loaded_plugins, prefix):
     if len(pairs) > number_of_rows:
         pairs = pairs[modulo_page * number_of_rows:number_of_rows * (modulo_page + 1)] + \
             [
-            (custom.Button.inline("Previous", data="{}_prev({})".format(prefix, modulo_page)),
-             custom.Button.inline("Next", data="{}_next({})".format(prefix, modulo_page)))
+            (custom.Button.inline("⏪", data="{}_prev({})".format(prefix, modulo_page)),
+             custom.Button.inline("⏩", data="{}_next({})".format(prefix, modulo_page)))
         ]
     return pairs
