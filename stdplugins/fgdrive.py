9246e1666c047719d5bb092b8487422004d0f012
Ravana <47528708+ravana69@users.noreply.github.com>
2019-09-28 19:19:15 +0530

    Update fgdrive.py

--- stdplugins/fgdrive.py
+++ stdplugins/fgdrive.py
@@ -44,7 +44,7 @@ async def _(event):
             "Uploading To G-Drive...\n\n█████████░",
             "Uploading To G-Drive Completed.\n\n██████████",
             "Generating G-Drive link........",
-            "Link Generated [G-Drive Link](https://drive.google.com/a/oxford.edu.in/uc?id=1hn_VuI6WdVlg6oZb3t1ch0dZ-qA&export=download)
+            "[G-Drive Link](https://drive.google.com/a/oxford.edu.in/uc?id=1hn_VuI6WdVlg6oZb3t1ch0dZ-qA&export=download)
  ]
 
         for i in animation_ttl:
