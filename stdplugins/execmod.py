97aca0bce8b3a0c946fa19050580b3d62e059df6
Ravana <47528708+ravana69@users.noreply.github.com>
2019-09-29 01:36:55 +0530

    Update execmod.py

--- stdplugins/execmod.py
+++ stdplugins/execmod.py
@@ -350,7 +350,7 @@ async def _(event):
     process = await asyncio.create_subprocess_shell(
         cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
     )
-    OUTPUT = f"**[Ravana's](tg://need_update_for_some_feature/) Telethon Updateded**\n"
+    OUTPUT = f"**[Ravana's](tg://need_update_for_some_feature/) Telethon Updated**\n"
     stdout, stderr = await process.communicate()
     if len(stdout) > Config.MAX_MESSAGE_SIZE_LIMIT:
         with io.BytesIO(str.encode(stdout)) as out_file:
