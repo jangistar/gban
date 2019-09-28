5ad87aea7cd10536c9603316fbb447baaf845317
Ravana <47528708+ravana69@users.noreply.github.com>
2019-09-28 19:03:12 +0530

    Create fgdrive.py

---
+++ stdplugins/fgdrive.py
@@ -0,0 +1,55 @@
+"""Emoji
+
+Available Commands:
+
+.fgdrive"""
+
+from telethon import events
+
+import asyncio
+
+
+
+
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 10
+
+    animation_ttl = range(0, 13)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "fgdrive":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+        
+            "Downloading To Heroku Started...\n\n░░░░░░░░░░",
+            "Downloading...\n\n█░░░░░░░░░",
+            "Downloading...\n\n███░░░░░░░",
+            "Downloading...\n\n█████░░░░░",
+            "Downloading...\n\n█████████░",    
+            "Download Completed.\n\n██████████",
+            "Uploading To G-Drive Started...\n\n░░░░░░░░░░",
+            "Uploading To G-Drive...\n\n█░░░░░░░░░",
+            "Uploading To G-Drive...\n\n█████░░░░░",
+            "Uploading To G-Drive...\n\n█████████░",
+            "Uploading To G-Drive Completed.\n\n██████████",
+            "Generating G-Drive link........",
+            "Link Generated : [G-Drive Link](https://drive.google.com/a/oxford.edu.in/uc?id=1hn_VuI6WdVlg6oZb3t1ch0dZ-qA&export=download)
+
+ ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 13])
