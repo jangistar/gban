9c8d4bded77ce35772940cfe756e93d52768bb7d
Ravana <47528708+ravana69@users.noreply.github.com>
2019-09-30 17:27:39 +0530

    Update letmefuck.py

--- stdplugins/letmefuck.py
+++ stdplugins/letmefuck.py
@@ -1,14 +1,15 @@
 """
 
-Let me Google / YouTube that for you! 
+Let me Google / YouTube / DuckDuckGo that for you! 
 
 Syntax:
 
  .lmg <search query>
 
  .lmy <search query>
+ 
+ .ddg <search query>
 
-By @Deonnn 
 
 
 
@@ -127,6 +128,51 @@ async def _(event):
 
 
         await event.edit("Let me **UThoob** that for you:\n👉 [{}]({})\n`Thank me later 😉` ".format(input_str,response_api.rstrip()))
+
+
+
+    else:
+
+
+
+        await event.edit("Something went wrong. Please try again later.")
+
+
+
+
+@borg.on(admin_cmd(pattern="ddg (.*)"))
+
+
+
+async def _(event):
+
+
+
+    if event.fwd_from:
+
+
+
+        return
+
+
+
+    input_str = event.pattern_match.group(1)
+
+
+
+    sample_url = "https://da.gd/s?url=https://duckduckgo.com/?q={}&t=h_&ia=about".format(input_str.replace(" ","+"))
+
+
+
+    response_api = requests.get(sample_url).text
+
+
+
+    if response_api:
+
+
+
+        await event.edit("Let me **duckduckgo** that for you:\n👉 [{}]({})\n`Thank me later 😉` ".format(input_str,response_api.rstrip()))
 
 
 
