880a0e3ce23725b636384a05c177d968d759533c
Ravana <47528708+ravana69@users.noreply.github.com>
2019-10-02 12:36:56 +0530

    Update weather.py

--- stdplugins/weather.py
+++ stdplugins/weather.py
@@ -55,7 +55,7 @@ async def _(event):
 async def _(event):
     if event.fwd_from:
         return
-    sample_url = "https://wttr.in/{}.webp"
+    sample_url = "https://wttr.in/{}.png"
     # logger.info(sample_url)
     input_str = event.pattern_match.group(1)
     async with aiohttp.ClientSession() as session:
