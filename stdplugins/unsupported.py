70c8068937a06ff782a9e4ce2613ae8e3e00c444
Ravana <47528708+ravana69@users.noreply.github.com>
2019-10-04 22:04:51 +0530

    Update unsupported.py

--- stdplugins/unsupported.py
+++ stdplugins/unsupported.py
@@ -1,4 +1,4 @@
-"""Command: .repo"""
+"""Command: .repo, .screenshottrick"""
 import asyncio
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
@@ -10,6 +10,22 @@ async def _(event):
     if event.fwd_from:
         return
     mentions = "**Link To The Custom Forked Repo:** https://github.com/ravana69/PornHub/ "
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f""
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete()
+
+@borg.on(admin_cmd("screenshottrick"))
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = ".eval NO_OF_SCSS = 5/n for I in range(NO_OF_SCSS):  /n await event.client(functions.messages.SendScreenshotNotificationRequest(peer=event.chat_id, reply_to_msg_id=42))/n await event.delete()"
     chat = await event.get_input_chat()
     async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
         mentions += f""
