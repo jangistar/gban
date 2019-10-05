45a92daa7be418e1272b4906d5510aade6ce1488
Ravana <47528708+ravana69@users.noreply.github.com>
2019-10-05 19:52:49 +0530

    Update autopic.py

--- stdplugins/autopic.py
+++ stdplugins/autopic.py
@@ -8,7 +8,7 @@ import shutil
 
 FONT_FILE_TO_USE = "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf"
 
-@command(pattern="^.autopic", outgoing=True)
+@borg.on(admin_cmd("auto ?(.*)"))
 async def autopic(event):
     downloaded_file_name = "userbot/original_pic.png"
     downloader = SmartDL(Var.DOWNLOAD_PFP_URL_CLOCK, downloaded_file_name, progress_bar=False)
