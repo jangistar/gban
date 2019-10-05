02519a855684a1d4c5ec79bd9daa284333a9a008
Ravana <47528708+ravana69@users.noreply.github.com>
2019-10-06 01:30:54 +0530

    Update autopic.py

--- stdplugins/autopic.py
+++ stdplugins/autopic.py
@@ -37,7 +37,7 @@ async def autopic(event):
         current_time = datetime.now().strftime("⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡ \n ⚡PORNHUB TIMEZONE⚡ \n  Time: %H:%M:%S \n  Date: %d.%m.%y \n⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡")
         img = Image.open(photo)
         drawn_text = ImageDraw.Draw(img)
-        fnt = ImageFont.truetype(FONT_FILE_TO_USE, 50)
+        fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
         drawn_text.text((30, 200), current_time, font=fnt, fill=(255, 255, 0))
         img.save(photo)
         file = await event.client.upload_file(photo)  # pylint:disable=E0602
