d034e03def7d6caedacf3429349294c88cbc9c48
Ravana <47528708+ravana69@users.noreply.github.com>
2019-10-06 13:25:42 +0530

    Update autopic.py

--- stdplugins/autopic.py
+++ stdplugins/autopic.py
@@ -38,7 +38,7 @@ async def autopic(event):
         img = Image.open(photo)
         drawn_text = ImageDraw.Draw(img)
         fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
-        drawn_text.text((90, 200), current_time, font=fnt, fill=(255, 255, 0))
+        drawn_text.text((90, 260), current_time, font=fnt, fill=(255, 255, 0))
         img.save(photo)
         file = await event.client.upload_file(photo)  # pylint:disable=E0602
         try:
