3229da6f583b6fd05fe000cb9077aaefdfd57ef2
Ravana <47528708+ravana69@users.noreply.github.com>
2019-10-06 19:44:33 +0530

    Update karbon.py

--- stdplugins/karbon.py
+++ stdplugins/karbon.py
@@ -57,7 +57,7 @@ async def carbon_api(e):
    sleep(5) #Waiting for downloading
 
    await e.edit("⬛⬛⬛⬛⬛")
-   file = './carbon.png'
+   file = './carbon.webp'
    await e.edit("✅Carbon Completed, Uploading Carbon✅")
    await e.client.send_file(
          e.chat_id,
@@ -67,6 +67,6 @@ async def carbon_api(e):
          reply_to=e.message.reply_to_msg_id,
          )
 
-   os.remove('./carbon.png')
+   os.remove('./carbon.webp')
    # Removing carbon.png after uploading
    await e.delete() # Deleting msg
