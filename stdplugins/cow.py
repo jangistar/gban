57debe9f3e6d30970cd82ff47bdc99ab8fe95212
Ravana <47528708+ravana69@users.noreply.github.com>
2019-10-06 21:08:13 +0530

    Update and rename Cow.py to cow.py

--- stdplugins/cow.py
+++ stdplugins/cow.py
@@ -5,14 +5,14 @@ from cowpy import cow
 from uniborg.util import admin_cmd
 
 
-@borg.on(events.NewMessage(pattern=r"^.(\.+)say (.*)", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"^.(\w+)say (.*)", outgoing=True))
 async def univsaye(cowmsg):
     """ For .cowsay module, uniborg wrapper for cow which says things. """
     if not cowmsg.text[0].isalpha() and cowmsg.text[0] not in ("/", "#", "@", "!"):
         arg = cowmsg.pattern_match.group(1).lower()
         text = cowmsg.pattern_match.group(2)
 
-        if arg == "kow":
+        if arg == "cow":
             arg = "default"
         if arg not in cow.COWACTERS:
             return
