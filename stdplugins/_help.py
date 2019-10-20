f490d680201c89692f93a469682665cbeafbde68
Ravana <47528708+ravana69@users.noreply.github.com>
2019-10-20 10:51:39 +0530

    Update _help.py

--- stdplugins/_help.py
+++ stdplugins/_help.py
@@ -14,11 +14,11 @@ async def _(event):
         s_help_string = borg._plugins[splugin_name].__doc__
     else:
         s_help_string = "****:"
-    help_string = """@UniBorg ( **Custom Built By** @r4v4n4 ) \n**Verified Account**: ✅\n**Official Website**: https://ravanaisdrunk.site.live\n
+    help_string = """@UniBorg ( **Custom Built By** @r4v4n4 ) \n**Verified Account**: ✅\n**Official Website**: [https://ravanaisdrunk.site.live](https://telegra.ph/file/44fd1b82f963b95fe1fb4.mp4)\n
 Pithun {}
 Talethrun {}
  
-**Custom Built Fork**: [https://github.com/ravana69/PornHub](https://telegra.ph/file/b0604ea53360cd3858ec5.mp4)""".format(
+**Custom Built Fork**: https://github.com/ravana69/PornHub""".format(
         sys.version,
         __version__
     )
