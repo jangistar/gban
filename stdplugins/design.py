ef7cf17e0fea079c718b9e17181a79ac095a5327
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-11-02 16:20:54 +0530

    Update design.py

--- stdplugins/design.py
+++ stdplugins/design.py
@@ -1,4 +1,4 @@
-"""COMMAND : .join , .pay , .fuck , .work , .push , .aag , .climb"""
+"""COMMAND : .join , .pay , .work , .push , .aag , .climb"""
 import asyncio
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
@@ -37,22 +37,6 @@ async def _(event):
         await event.reply(mentions)
     await event.delete()
 
-
-@borg.on(admin_cmd("fuck"))
-async def _(event):
-    if event.fwd_from:
-        return
-    mentions = "`......................................../´¯/) \n......................................,/¯../ \n...................................../..../ \n..................................../´.¯/\n..................................../´¯/\n..................................,/¯../ \n................................../..../ \n................................./´¯./\n................................/´¯./\n..............................,/¯../ \n............................./..../ \n............................/´¯/\n........................../´¯./\n........................,/¯../ \n......................./..../ \n....................../´¯/\n....................,/¯../ \n.................../..../ \n............./´¯/'...'/´¯¯`·¸ \n........../'/.../..../......./¨¯\ \n........('(...´...´.... ¯~/'...') \n.........\.................'...../ \n..........''...\.......... _.·´ \n............\..............( \n..............\.............\...`"
-    chat = await event.get_input_chat()
-    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
-        mentions += f""
-    reply_message = None
-    if event.reply_to_msg_id:
-        reply_message = await event.get_reply_message()
-        await reply_message.reply(mentions)
-    else:
-        await event.reply(mentions)
-    await event.delete()
 
 
 @borg.on(admin_cmd("climb"))
