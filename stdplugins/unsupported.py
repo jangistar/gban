b8a2a1bcbcac4d4ec3ebdc6d3fc223462d1e33b9
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-11-04 03:30:34 +0530

    Update unsupported.py

--- stdplugins/unsupported.py
+++ stdplugins/unsupported.py
@@ -9,7 +9,7 @@ from uniborg.util import admin_cmd
 async def _(event):
     if event.fwd_from:
         return
-    mentions = "**Link To The Custom Uniborg Repo:** https://github.com/mkaraniya/BotHub/ "
+    mentions = "**Link To The Custom Uniborg Repo:** https://github.com/mkaraniya/BotHub "
     chat = await event.get_input_chat()
     async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
         mentions += f""
