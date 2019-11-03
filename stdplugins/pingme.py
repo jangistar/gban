d9842283b7c167243b8fb3b6a22b0f00c9476ee1
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-11-03 16:50:13 +0530

    Update and rename ping.py to pingme.py

--- stdplugins/pingme.py
+++ stdplugins/pingme.py
@@ -1,11 +1,11 @@
-""" Command: .porn """
+""" Command: .pingme """
 
 from telethon import events
 from datetime import datetime
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd(pattern="porn ?(.*)", allow_sudo=True))
+@borg.on(admin_cmd(pattern="pingme ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
