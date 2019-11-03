d08a9019cd97ebec93942781a0052552a69bf46f
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-11-03 16:54:41 +0530

    Create pingbrog.py

---
+++ stdplugins/pingbrog.py
@@ -0,0 +1,16 @@
+""" `.ping' to getyour ping"""
+
+from telethon import events
+from datetime import datetime
+from uniborg.util import admin_cmd
+
+
+@borg.on(admin_cmd("ping"))
+async def _(event):
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    await event.edit("🇵 I 🇳 🇬 👇")
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    await event.edit("`🇵 I 🇳 🇬 👇`\n`{}ms`".format(ms))
