9155e236491b6f87048379077c480d0bc7cf90e8
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-11-03 17:00:12 +0530

    Update profilepicgrab.py
    #51 id

--- stdplugins/profilepicgrab.py
+++ stdplugins/profilepicgrab.py
@@ -46,9 +46,9 @@ async def _(event):
     except Exception as e:
         dc_id = "Need a Profile Picture to check **this**"
         location = str(e)
-    caption = """Profile Pic Grabber By @r4v4n4
+    caption = """Profile Pic Grabber By @Mayur_Karaniya
 
-Rendi: <a href='tg://user?id={}'>{}</a>
+Owner: <a href='tg://user?name={}'>{}</a>
 """.format(
         user_id,
         user_id,
