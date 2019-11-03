545096e21a25fe979704b9c8035cd41bbf041da4
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-11-04 02:02:41 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -63,8 +63,9 @@ class Config(object):
     # foloowing plugins won't work on Heroku,
     # ⚡Remove This To Make Them Work But Would Make Bot Unstable AF...⚡
     NO_LOAD = [
-        "pmpermit"
-
+                    "pmpermit" ,
+                    "left" ,
+                    "hand" ,   
     ]
     # Get your own API key from https://www.remove.bg/ or
     # feel free to use http://telegram.dog/Remove_BGBot
