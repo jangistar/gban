cc1b008a596de6a1af7b26f9782b1588b4907f40
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-11-04 01:47:56 +0530

    Update sysd.py

--- stdplugins/sysd.py
+++ stdplugins/sysd.py
@@ -14,9 +14,9 @@ async def sysdetails(sysd):
     """ a. """
     if not sysd.text[0].isalpha() and sysd.text[0] not in ("/", "#", "@", "!"):
         try:
-            neo = "neofetch/neofetch --off --color_blocks off --bold off --cpu_temp C \
-                    --gpu_brand=on --refresh_rate=on --gtk_shorthand=on --colors=distro  --backend=ascii \
-                    --source=auto --Redhat source --cpu_speed on --cpu_cores physical --kernel_shorthand off --stdout"
+            neo = "neofetch/neofetch --on --color_blocks on --bold on --cpu_temp C \
+                    --gpu_brand on --refresh_rate on --gtk_shorthand on --colors distro  --backend ascii \
+                    --source auto --Redhat source --cpu_speed on --cpu_cores physical --kernel_shorthand on --stdout"
             fetch = await asyncrunapp(
                 neo,
                 stdout=asyncPIPE,
