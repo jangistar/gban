36be196e7e49d867b05bfc4154bced66b9012299
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-11-05 18:05:21 +0530

    Update sysd.py

--- stdplugins/sysd.py
+++ stdplugins/sysd.py
@@ -14,10 +14,10 @@ async def sysdetails(sysd):
     """ a. """
     if not sysd.text[0].isalpha() and sysd.text[0] not in ("/", "#", "@", "!"):
         try:
-            neo = "neofetch/neofetch --off --color_blocks on --bold on --cpu_temp C \
+            neo = "neofetch/neofetch --off --color_blocks on --bold on --cpu_temp=C \
                     --cpu_speed on --cpu_cores physical --kernel_shorthand on \
-                    --gpu_brand on --refresh_rate on --gtk_shorthand on --colors distro  --backend ascii \
-                    --source auto --Redhat source --stdout"
+                    --gpu_brand=on --refresh_rate=on --gtk_shorthand=on --colors=distro  --backend=ascii \
+                    --source=auto --Redhat source --stdout"
             fetch = await asyncrunapp(
                 neo,
                 stdout=asyncPIPE,
@@ -30,5 +30,5 @@ async def sysdetails(sysd):
 
             await sysd.edit("Neofetch Result: `" + result + "`")
         except FileNotFoundError:
-            await sysd.edit("`Hello, on mkaraniya/BotHub install .neofetch first`")
+            await sysd.edit("`Hello, on mkaraniya/PepeBot install .neofetch first kthx`")
 
