fbde66c99eac0c129543406223db46d05ae520ae
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-11-05 19:09:10 +0530

    Update anime_downloader.py

--- stdplugins/anime_downloader.py
+++ stdplugins/anime_downloader.py
@@ -1,10 +1,7 @@
-"""
-Anime Batch Downloader Plugin for userbot. //set TEMP_DIR Env Var first.
+"""Anime Batch Downloader Plugin for userbot. //set TEMP_DIR Env Var first.
 usage:- get a link of Animefrenzy.net Anime page and use in cmd.
-cmd:- .anime page_link
-By:- @Zero_cool7870	  
+cmd:- .anime page_link"""
 
-"""
 from telethon import events
 import asyncio
 from bs4 import BeautifulSoup as bs 
