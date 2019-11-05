f3f09e8481bfcdf66509303a676d4c5d219d3508
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-11-06 01:37:36 +0530

    Update calendar.py

--- stdplugins/calendar.py
+++ stdplugins/calendar.py
@@ -24,7 +24,7 @@ async def _(event):
         response_content = requests.get(required_url, headers=headers).json()
         a = ""
         if "error" not in response_content:
-            current_date_detail_arraays = response_content["months"][0]["days"][0]
+            current_date_detail_arraays = response_content["month"][0]["day"][0]
             a = json.dumps(current_date_detail_arraays, sort_keys=True, indent=4)
         else:
             a = response_content["error"]
