21d7623f09a89eeb703391c2c3a39a3e502fee9f
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-11-06 16:49:00 +0530

    Update misc.py

--- stdplugins/misc.py
+++ stdplugins/misc.py
@@ -1,4 +1,49 @@
 
+"""cmd : .sguide """
+
+from telethon import events
+
+import asyncio
+
+
+
+
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.8
+
+    animation_ttl = range(0, 1)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "sguide":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "[Setup Guide - Basic](https://telegra.ph/How-to-host-a-Telegram-Userbot-11-06)",
+            
+            "[BotHub's README.md file](https://github.com/mkaraniya/BotHub/blob/master/README.md)"
+            
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 3])
+
+
+
+
 """cmd : .readme """
 
 from telethon import events
@@ -19,7 +64,7 @@ async def _(event):
 
     animation_interval = 0.8
 
-    animation_ttl = range(0, 2)
+    animation_ttl = range(0, 1)
 
     input_str = event.pattern_match.group(1)
 
@@ -29,9 +74,10 @@ async def _(event):
 
         animation_chars = [
 
-            "[Setup Guide - Basic](https://telegra.ph/How-to-host-a-Telegram-Userbot-11-06)",
+                       
+            "[BotHub's README.md file](https://github.com/mkaraniya/BotHub/blob/master/README.md)",
             
-            "[BotHub's README.md file](https://github.com/mkaraniya/BotHub/blob/master/README.md)"
+            "[Setup Guide - Basic](https://telegra.ph/How-to-host-a-Telegram-Userbot-11-06)"
             
         ]
 
@@ -40,6 +86,10 @@ async def _(event):
             await asyncio.sleep(animation_interval)
 
             await event.edit(animation_chars[i % 3])
+
+
+
+
 
 
 
