36a1813e2e4257b77a0b3e3d60cd904cf07c2086
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-11-07 00:32:14 +0530

    Update ninja.py

--- stdplugins/ninja.py
+++ stdplugins/ninja.py
@@ -1,6 +1,9 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+""" i just imported it from Uniborg , i can't figure out the use of this module thou."""
+
+
 
 import asyncio
 
