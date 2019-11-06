4c3dd36fa68ebb43d619ebe79f374ace71194029
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-11-07 01:09:33 +0530

    Update power_tools.py

--- stdplugins/power_tools.py
+++ stdplugins/power_tools.py
@@ -1,7 +1,5 @@
 """Restart or Terminate the bot from any chat
-Available Commands:
-.restart
-.shutdown"""
+.restart, .shutdown, .fastboot, .reboot, .asciiboot"""
 # This Source Code Form is subject to the terms of the GNU
 # General Public License, v.3.0. If a copy of the GPL was not distributed with this
 # file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.en.html
