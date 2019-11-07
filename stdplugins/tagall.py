def16419023a5dfe73ed1becf4545ff45b666eac
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-11-08 00:14:31 +0530

    Update tagall.py

--- stdplugins/tagall.py
+++ stdplugins/tagall.py
@@ -1,3 +1,5 @@
+"""cmd .tagall to tagall members of group"""
+
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
