56ac7eaff069e40b559609bc8f17ffb9c65c9873
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-11-07 22:36:45 +0530

    Create cp.py

---
+++ stdplugins/cp.py
@@ -0,0 +1,56 @@
+import asyncio
+import random
+import re
+import time
+
+from collections import deque
+
+import requests
+
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
+from telethon import events
+
+from uniborg.util import admin_cmd
+
+
+# ================= CONSTANT =================
+DISABLE_ROON = False
+# ===========================================
+
+
+
+@borg.on(admin_cmd(pattern="cp(?: |$)(.*)", outgoing=True)) 
+async def copypasta(cp_e):
+    """ Copypasta the famous meme """
+    if not cp_e.text[0].isalpha() and cp_e.text[0] not in ("/", "#", "@", "!"):
+        textx = await cp_e.get_reply_message()
+        message = cp_e.pattern_match.group(1)
+
+        if message:
+            pass
+        elif textx:
+            message = textx.text
+        else:
+            await cp_e.edit("`😂🅱️IvE👐sOME👅text👅for✌️Me👌tO👐MAkE👀iT💞funNy!💦`")
+            return
+
+        reply_text = random.choice(EMOJIS)
+        b_char = random.choice(
+            message
+        ).lower()  # choose a random character in the message to be substituted with 🅱️
+        for owo in message:
+            if owo == " ":
+                reply_text += random.choice(EMOJIS)
+            elif owo in EMOJIS:
+                reply_text += owo
+                reply_text += random.choice(EMOJIS)
+            elif owo.lower() == b_char:
+                reply_text += "🅱️"
+            else:
+                if bool(random.getrandbits(1)):
+                    reply_text += owo.upper()
+                else:
+                    reply_text += owo.lower()
+        reply_text += random.choice(EMOJIS)
+        await cp_e.edit(reply_text)
