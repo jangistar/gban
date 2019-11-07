147a9a4f4f47b8e8808b80ca55e5198e08ab02b3
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-11-08 01:44:59 +0530

    Create thinklol.py
    funny lols

---
+++ stdplugins/thinklol.py
@@ -0,0 +1,18 @@
+# (c) @UniBorg
+"""cmd .tlol"""
+
+from telethon import events
+import asyncio
+from collections import deque
+
+
+@borg.on(events.NewMessage(pattern=r"\.tlol", outgoing=True))
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("🤔🧐🤔🧐🤔🧐"))
+	for _ in range(999):
+		await asyncio.sleep(0.1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
+    
