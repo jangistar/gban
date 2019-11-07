5b43e7c24168bcd5c4acbdd9ff0e85def792f0a0
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-11-08 02:53:39 +0530

    Update promote.py
    now working

--- stdplugins/promote.py
+++ stdplugins/promote.py
@@ -1,19 +1,18 @@
-"""Reply to a user to .premote them in the current chat
-and to just prank do .prankpremote"""
+"""Reply to a user to .promote them in the current chat"""
 from telethon import events
-import asyncio,re
+import asyncio
 from datetime import datetime
 from telethon.tl.functions.channels import EditAdminRequest
 from telethon.tl.types import ChatAdminRights
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("premote ?(.*)"))
+@borg.on(admin_cmd(pattern="promote ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
     start = datetime.now()
-    to_premote_id = None
+    to_promote_id = None
     rights = ChatAdminRights(
         change_info=False,
         post_messages=True,
@@ -24,52 +23,40 @@ async def _(event):
         pin_messages=True,
         add_admins=False,
     )
-    rank = await event.get_user()
-    if not rank:
-        # Just in case.
-        rank = "admin"
-    else:
-        return
     input_str = event.pattern_match.group(1)
     reply_msg_id = event.message.id
     if reply_msg_id:
         r_mesg = await event.get_reply_message()
-        to_premote_id = r_mesg.sender_id
+        to_promote_id = r_mesg.sender_id
     elif input_str:
-        to_premote_id = input_str
+        to_promote_id = input_str
     try:
-        await borg(EditAdminRequest(event.chat_id, to_premote_id, rights, rank))
+        await borg(EditAdminRequest(event.chat_id, to_promote_id, rights, ""))
     except (Exception) as exc:
         await event.edit(str(exc))
     else:
-        await event.edit("Successfully premoted")
+        await event.edit("Successfully Promoted")
 
 
-@borg.on(admin_cmd("prankpremote ?(.*)"))
+@borg.on(admin_cmd(pattern="prankpromote ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
     start = datetime.now()
-    to_premote_id = None
+    to_promote_id = None
     rights = ChatAdminRights(
         post_messages=True
     )
-    rank = await event.get_user()
-    if not rank:
-        # Just in case.
-        rank = "admin"
-    else:
-        return
     input_str = event.pattern_match.group(1)
     reply_msg_id = event.message.id
     if reply_msg_id:
         r_mesg = await event.get_reply_message()
-        to_premote_id = r_mesg.sender_id
+        to_promote_id = r_mesg.sender_id
     elif input_str:
-        to_premote_id = input_str
+        to_promote_id = input_str
     try:
-        await borg(EditAdminRequest(event.chat_id, to_premote_id, rights, rank))
+        await borg(EditAdminRequest(event.chat_id, to_promote_id, rights, ""))
     except (Exception) as exc:
         await event.edit(str(exc))
     else:
-        await event.edit("Successfully premoted")
+        await event.edit("Successfully Promoted")
