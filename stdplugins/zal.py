fc117e2af47b7e53e7be69e0d7b1fea335577a10
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-11-07 16:49:37 +0530

    Create zal.py

---
+++ stdplugins/zal.py
@@ -0,0 +1,194 @@
+"""use .zal Invoke the feeling of chaos."""
+
+import asyncio
+import random
+import re
+import time
+
+from collections import deque
+
+import requests
+
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
+from telethon import events
+
+from uniborg.util import admin_cmd
+
+
+# ================= CONSTANT =================
+
+
+ZALG_LIST = [["̖",
+              " ̗",
+              " ̘",
+              " ̙",
+              " ̜",
+              " ̝",
+              " ̞",
+              " ̟",
+              " ̠",
+              " ̤",
+              " ̥",
+              " ̦",
+              " ̩",
+              " ̪",
+              " ̫",
+              " ̬",
+              " ̭",
+              " ̮",
+              " ̯",
+              " ̰",
+              " ̱",
+              " ̲",
+              " ̳",
+              " ̹",
+              " ̺",
+              " ̻",
+              " ̼",
+              " ͅ",
+              " ͇",
+              " ͈",
+              " ͉",
+              " ͍",
+              " ͎",
+              " ͓",
+              " ͔",
+              " ͕",
+              " ͖",
+              " ͙",
+              " ͚",
+              " ",
+              ],
+             [" ̍",
+              " ̎",
+              " ̄",
+              " ̅",
+              " ̿",
+              " ̑",
+              " ̆",
+              " ̐",
+              " ͒",
+              " ͗",
+              " ͑",
+              " ̇",
+              " ̈",
+              " ̊",
+              " ͂",
+              " ̓",
+              " ̈́",
+              " ͊",
+              " ͋",
+              " ͌",
+              " ̃",
+              " ̂",
+              " ̌",
+              " ͐",
+              " ́",
+              " ̋",
+              " ̏",
+              " ̽",
+              " ̉",
+              " ͣ",
+              " ͤ",
+              " ͥ",
+              " ͦ",
+              " ͧ",
+              " ͨ",
+              " ͩ",
+              " ͪ",
+              " ͫ",
+              " ͬ",
+              " ͭ",
+              " ͮ",
+              " ͯ",
+              " ̾",
+              " ͛",
+              " ͆",
+              " ̚",
+              ],
+             [" ̕",
+              " ̛",
+              " ̀",
+              " ́",
+              " ͘",
+              " ̡",
+              " ̢",
+              " ̧",
+              " ̨",
+              " ̴",
+              " ̵",
+              " ̶",
+              " ͜",
+              " ͝",
+              " ͞",
+              " ͟",
+              " ͠",
+              " ͢",
+              " ̸",
+              " ̷",
+              " ͡",
+              ]]
+
+# DISABLE_ROON = False
+# ===========================================
+
+
+@borg.on(events.NewMessage(outgoing=True, pattern="^:/$")) 
+async def kek(keks):
+    """ Check yourself ;)"""
+    uio = ["/", "\\"]
+    for i in range(1, 15):
+        time.sleep(0.3)
+        await keks.edit(":" + uio[i % 2])
+
+
+
+@borg.on(events.NewMessage(outgoing=True, pattern="^;_;$"))
+async def fun(e):
+    t = ";__;"
+    for j in range(10):
+        t = t[:-1] + "_;"
+        await e.edit(t)
+        
+
+
+@borg.on(admin_cmd(pattern="izal", outgoing=True)) 
+async def izal(zgfy):
+    """ Invoke the feeling of chaos. """
+    if not zgfy.text[0].isalpha() and zgfy.text[0] not in ("/", "#", "@", "!"):
+        reply_text = list()
+        textx = await zgfy.get_reply_message()
+        message = zgfy.pattern_match.group()
+        if message:
+            pass
+        elif textx:
+            message = textx.text
+        else:
+            await zgfy.edit(
+                "`gͫ ̆ i̛ ̺ v͇̆ ȅͅ   a̢ͦ   s̴̪ c̸̢ ä̸ rͩͣ y͖͞   t̨͚ é̠ x̢͖  t͔͛`"
+            )
+            return
+
+        for charac in message:
+            if not charac.isalpha():
+                reply_text.append(charac)
+                continue
+
+            for _ in range(0, 3):
+                randint = random.randint(0, 2)
+
+                if randint == 0:
+                    charac = charac.strip() + \
+                        random.choice(ZALG_LIST[0]).strip()
+                elif randint == 1:
+                    charac = charac.strip() + \
+                        random.choice(ZALG_LIST[1]).strip()
+                else:
+                    charac = charac.strip() + \
+                        random.choice(ZALG_LIST[2]).strip()
+
+            reply_text.append(charac)
+
+        await zgfy.edit("".join(reply_text))
+
