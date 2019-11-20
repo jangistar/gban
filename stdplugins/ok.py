752dd400e73901e82a8ee757dfb6a8ef56354b80
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-11-20 23:59:39 +0530

    Create ok.py

---
+++ stdplugins/ok.py
@@ -0,0 +1,45 @@
+""" displays the Truth
+use cmd .ok"""
+
+from telethon import events
+
+import asyncio
+
+from uniborg.util import admin_cmd
+
+@borg.on(admin_cmd("(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.00001
+    animation_ttl = range(0, 90)
+    input_str = event.pattern_match.group(1)
+    if input_str == "ok":
+        await event.edit(input_str)
+        animation_chars = [
+            "B",
+            "O",
+            "T",
+            "H",
+            "U",
+            "B",
+            " ",
+            "I",
+            "S",
+            " ",
+            "T",
+            "H",
+            "E",
+            " ",
+            "B",
+            "E",
+            "S",
+            "T",
+            " ",
+            "Ok Sar 😇"
+        ]
+
+        for i in animation_ttl:
+        	
+            await asyncio.sleep(animation_interval)
+            await event.edit(animation_chars[i % 18])
