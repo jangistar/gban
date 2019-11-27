a72cb7b2ec1d16cd5fb42484403dd0e27750085d
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-11-27 12:41:57 +0530

    Create spamV2.py

---
+++ stdplugins/spamV2.py
@@ -0,0 +1,13 @@
+""" tspam"""
+import asyncio
+import time
+from asyncio import wait
+from uniborg.util import admin_cmd
+
+@borg.on(admin_cmd("tspam"))
+async def tmeme(e):
+     tspam = str(e.text[7:])
+     message = tspam.replace(" ", "")
+     for letter in message:
+         await e.respond(letter)
+     await e.delete()
