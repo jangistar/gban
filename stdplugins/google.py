fa95676f3a1c563b6785aa640ee8c45d2b283261
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-11-27 14:33:44 +0530

    Update google.py

--- stdplugins/google.py
+++ stdplugins/google.py
@@ -20,7 +20,7 @@ def progress(current, total):
     logger.info("Downloaded {} of {}\nCompleted {}".format(current, total, (current / total) * 100))
 
 
-@borg.on(admin_cmd(pattern="gs ?(.*)", allow_sudo=True))
+@borg.on(admin_cmd(pattern="gs (.*)", allow_sudo=True)) # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
