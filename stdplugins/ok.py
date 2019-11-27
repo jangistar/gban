0f0f7f8420f23abc85369e5ce5029d0a79145112
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-11-27 15:05:41 +0530

    Update ok.py

--- stdplugins/ok.py
+++ stdplugins/ok.py
@@ -11,7 +11,7 @@ from uniborg.util import admin_cmd
 async def _(event):
     if event.fwd_from:
         return
-    animation_interval = 0.00001
+    animation_interval = 0.05
     animation_ttl = range(0, 90)
     input_str = event.pattern_match.group(1)
     if input_str == "ok":
@@ -23,23 +23,19 @@ async def _(event):
             "H",
             "U",
             "B",
-            " ",
             "I",
             "S",
-            " ",
             "T",
             "H",
             "E",
-            " ",
             "B",
             "E",
             "S",
             "T",
-            " ",
-            "Ok Sar 😇"
+            "BOTHUB IS THE BEST , 0K Sir 😇"
         ]
 
         for i in animation_ttl:
         	
             await asyncio.sleep(animation_interval)
-            await event.edit(animation_chars[i % 18])
+            await event.edit(animation_chars[i % 78])
