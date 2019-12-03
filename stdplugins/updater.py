26ab700a8788412f9f3c16fd9cb50411dd9c1a25
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-12-03 13:39:34 +0530

    Update updater.py

--- stdplugins/updater.py
+++ stdplugins/updater.py
@@ -125,10 +125,11 @@ async def upstream(ups):
 
 
 
-CMD_HELP.update({
+"""CMD_HELP.update({
     'update':
     ".update\
 \nUsage: Checks if the main userbot repository has any updates and shows a changelog if so.\
 \n\n.update now\
 \nUsage: Updates your userbot, if there are any updates in the main userbot repository."
 })
+"""
