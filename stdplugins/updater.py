70545dd4da8a480b327c27422af9ad7709f0c0b7
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-12-04 15:33:55 +0530

    Update updater.py

--- stdplugins/updater.py
+++ stdplugins/updater.py
@@ -5,15 +5,15 @@ from os import remove
 from os import execl
 import sys
 
-from git import Repo
-from git.exc import GitCommandError
-from git.exc import InvalidGitRepositoryError
-from git.exc import NoSuchPathError
+# from git import Repo
+# from git.exc import GitCommandError
+# from git.exc import InvalidGitRepositoryError
+# from git.exc import NoSuchPathError
 
-# from uniborg import CMD_HELP, bot
+# from .. import bot
 # from uniborg.events import register
 
-
+import git
 import asyncio
 import random
 import re
@@ -29,7 +29,7 @@ from telethon import events
 
 from uniborg.util import admin_cmd
 
-import git
+
 from contextlib import suppress
 import os
 import sys
@@ -146,7 +146,7 @@ async def updater(message):
                     remote.set_url(heroku_git_url)
                 else:
                     remote = repo.create_remote("heroku", heroku_git_url)
-                asyncio.get_event_loop().create_task(deploy_start(bot, message, HEROKU_GIT_REF_SPEC, remote))
+                asyncio.get_event_loop().create_task(deploy_start(tgbot, message, HEROKU_GIT_REF_SPEC, remote))
 
             else:
                 await message.edit("Please create the var HEROKU_APP_NAME as the key and the name of your bot in heroku as your value.")
@@ -164,11 +164,11 @@ def generate_change_log(git_repo, diff_marker):
         out_put_str += f"•[{repo_change.committed_datetime.strftime(d_form)}]: {repo_change.summary} <{repo_change.author}>\n"
     return out_put_str
 
-async def deploy_start(bot, message, refspec, remote):
+async def deploy_start(tgbot, message, refspec, remote):
     await message.edit(RESTARTING_APP)
-    await message.edit("restarted! do `.ping` to check if I am online?")
+    await message.edit("restarted! do `.ping` to check if I am pinging?")
     await remote.push(refspec=refspec)
-    await bot.disconnect()
+    await tgbot.disconnect()
     os.execl(sys.executable, sys.executable, *sys.argv)
 
     
