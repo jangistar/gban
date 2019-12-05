6528fe88bb336c06217150a40cbf7f3850523813
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-12-06 04:23:14 +0530

    Update updater.py

--- stdplugins/updater.py
+++ stdplugins/updater.py
@@ -42,7 +42,7 @@ IS_SELECTED_DIFFERENT_BRANCH = (
     "in this case, Updater is unable to identify the branch to be updated."
     "please check out to an official branch, and re-start the updater."
 )
-OFFICIAL_UPSTREAM_REPO = "https://www.github.com/mkaraniya/BotHub.git"
+OFFICIAL_UPSTREAM_REPO = "https://www.github.com/mkaraniya/BotHub_TeKnoways.git"
 BOT_IS_UP_TO_DATE = "the userbot is up-to-date."
 NEW_BOT_UP_DATE_FOUND = (
     "new update found for {branch_name}\n"
@@ -53,7 +53,7 @@ NEW_UP_DATE_FOUND = (
     "new update found for {branch_name}\n"
     "updating ..."
 )
-REPO_REMOTE_NAME = "git"
+REPO_REMOTE_NAME = "bothub"
 IFFUCI_ACTIVE_BRANCH_NAME = "master"
 DIFF_MARKER = "HEAD..{remote_name}/{branch_name}"
 NO_HEROKU_APP_CFGD = "no heroku application found, but a key given? 😕 "
