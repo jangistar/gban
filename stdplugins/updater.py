258fcfa7630e825bc156bca32e6de9d953da5bcd
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-12-08 13:20:40 +0530

    Update updater.py

--- stdplugins/updater.py
+++ stdplugins/updater.py
@@ -61,12 +61,6 @@ HEROKU_GIT_REF_SPEC = "HEAD:refs/heads/master"
 RESTARTING_APP = "re-starting heroku application"
 # -- Constants End -- #
 
-async def generate_change_log(git_repo, diff_marker):
-    out_put_str = ""
-    d_form = "%d/%m/%y"
-    for repo_change in git_repo.iter_commits(diff_marker):
-        out_put_str += f"•[{repo_change.committed_datetime.strftime(d_form)}]: {repo_change.summary} <{repo_change.author}>\n"
-    return out_put_str
 
 @borg.on(admin_cmd("update ?(.*)", outgoing=True))
 async def updater(message):
@@ -118,7 +112,7 @@ async def updater(message):
     if len(message_one) > 4095:
         with open("change.log", "w+", encoding="utf8") as out_file:
             out_file.write(str(message_one))
-        await borg.send_message(
+        await tgbot.send_message(
             message.chat_id,
             document="change.log",
             caption=message_two
@@ -128,7 +122,7 @@ async def updater(message):
         await message.edit(message_one)
 
     temp_upstream_remote.fetch(active_branch_name)
-  # repo.git.reset("--hard", "FETCH_HEAD")
+    repo.git.reset("--hard", "FETCH_HEAD")
 
     if Config.HEROKU_API_KEY is not None:
         import heroku3
@@ -162,6 +156,13 @@ async def updater(message):
     else:
         await message.edit("No heroku api key found in HEROKU_API_KEY var")
         
+
+def generate_change_log(git_repo, diff_marker):
+    out_put_str = ""
+    d_form = "%d/%m/%y"
+    for repo_change in git_repo.iter_commits(diff_marker):
+        out_put_str += f"•[{repo_change.committed_datetime.strftime(d_form)}]: {repo_change.summary} <{repo_change.author}>\n"
+    return out_put_str
 
 async def deploy_start(tgbot, message, refspec, remote):
     await message.edit(RESTARTING_APP)
