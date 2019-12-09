ce2e14e3df4642aae00c1d19fa1a26d3e9932c44
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-12-10 00:32:56 +0530

    Update changelog.py

--- stdplugins/changelog.py
+++ stdplugins/changelog.py
@@ -36,7 +36,7 @@ async def is_off_br(br):
     return
 
 
-@borg.on(admin_cmd("^.chk (?: |$)(.*)", outgoing=True, allow_sudo=True))
+@borg.on(admin_cmd("chk ?(.*)", outgoing=True, allow_sudo=True))
 async def upstream(ups):
     "For .update command, check if the bot is up to date, update if specified"
     await ups.edit("`Checking for updates, please wait....`")
