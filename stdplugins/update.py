6efba0e213d3af2b17ebff4efd75786b53ee16ec
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-12-09 19:25:11 +0530

    Update and rename update to update.py

--- stdplugins/update.py
+++ stdplugins/update.py
@@ -36,22 +36,22 @@ import sys
 import asyncio
 
 async def gen_chlog(repo, diff):
-    ch_log = ''
+    ch_log = ""
     d_form = "%d/%m/%y"
     for c in repo.iter_commits(diff):
-        ch_log += f'•[{c.committed_datetime.strftime(d_form)}]: {c.summary} <{c.author}>\n'
+        ch_log += f"•[{c.committed_datetime.strftime(d_form)}]: {c.summary} <{c.author}>\n"
     return ch_log
 
 
 async def is_off_br(br):
-    off_br = ['master']
+    off_br = ["master"]
     for k in off_br:
         if k == br:
             return 1
     return
 
 
-@borg.on(admin_cmd("update ?(.*)", outgoing=True, allow_sudo=True))
+@borg.on(admin_cmd("chk ?(.*)", outgoing=True, allow_sudo=True))
 async def upstream(ups):
     "For .update command, check if the bot is up to date, update if specified"
     await ups.edit("`Checking for updates, please wait....`")
@@ -62,40 +62,40 @@ async def upstream(ups):
         txt = "`Oops.. Updater cannot continue due to some problems occured`\n\n**LOGTRACE:**\n"
         repo = Repo()
     except NoSuchPathError as error:
-        await ups.edit(f'{txt}\n`directory {error} is not found`')
+        await ups.edit(f"{txt}\n`directory {error} is not found`")
         return
     except InvalidGitRepositoryError as error:
         await ups.edit(
-            f'{txt}\n`directory {error} does not seems to be a git repository`'
+            f"{txt}\n`directory {error} does not seems to be a git repository`"
         )
         return
     except GitCommandError as error:
-        await ups.edit(f'{txt}\n`Early failure! {error}`')
+        await ups.edit(f"{txt}\n`Early failure! {error}`")
         return
 
     ac_br = repo.active_branch.name
     if not await is_off_br(ac_br):
         await ups.edit(
-            f'**[UPDATER]:**` Looks like you are using your own custom branch ({ac_br}). \
+            f"**[UPDATER]:**` Looks like you are using your own custom branch ({ac_br}). \
             in that case, Updater is unable to identify which branch is to be merged. \
-            please checkout to any official branch`')
+            please checkout to any official branch`")
         return
 
     try:
-        repo.create_remote('upstream', off_repo)
+        repo.create_remote("upstream", off_repo)
     except BaseException:
         pass
 
-    ups_rem = repo.remote('upstream')
+    ups_rem = repo.remote("upstream")
     ups_rem.fetch(ac_br)
-    changelog = await gen_chlog(repo, f'HEAD..upstream/{ac_br}')
+    changelog = await gen_chlog(repo, f"HEAD..upstream/{ac_br}")
 
     if not changelog:
-        await ups.edit(f'\n`Your BOT is` **up-to-date** `with` **{ac_br}**\n')
+        await ups.edit(f"\n`Your BOT is` **up-to-date** `with` **{ac_br}**\n")
         return
 
     if conf != "now":
-        changelog_str = f'**New UPDATE available for [{ac_br}]:\n\nCHANGELOG:**\n`{changelog}`'
+        changelog_str = f"**New UPDATE available for [{ac_br}]:\n\nCHANGELOG:**\n`{changelog}`"
         if len(changelog_str) > 4096:
             await ups.edit("`Changelog is too big, sending it as a file.`")
             file = open("output.txt", "w+")
@@ -113,11 +113,11 @@ async def upstream(ups):
             "`do \".update now\" to update\nDon't if using Heroku`")
         return
 
-    await ups.edit('`New update found, updating...`')
+    await ups.edit("`New update found, updating...`")
     ups_rem.fetch(ac_br)
-    repo.git.reset('--hard', 'FETCH_HEAD')
-    await ups.edit('`Successfully Updated!\n'
-                   'Bot is restarting... Wait for a second!`')
+    repo.git.reset("--hard', 'FETCH_HEAD")
+    await ups.edit("`Successfully Updated!\n"
+                   "Bot is restarting... Wait for a second!`")
     await bot.disconnect()
     # Spin a new instance of bot
     execl(sys.executable, sys.executable, *sys.argv)
