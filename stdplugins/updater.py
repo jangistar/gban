ae195edb6e9c99bc43e932c836072f2d9cb1a776
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-12-09 12:47:51 +0530

    Update updater.py

--- stdplugins/updater.py
+++ stdplugins/updater.py
@@ -51,7 +51,7 @@ async def is_off_br(br):
     return
 
 
-@borg.on(admin_cmd, outgoing=True,("update ?(.*)", allow_sudo=True))
+@borg.on(admin_cmd, outgoing=True, allow_sudo=True("update ?(.*)"))
 async def upstream(ups):
     "For .update command, check if the bot is up to date, update if specified"
     await ups.edit("`Checking for updates, please wait....`")
