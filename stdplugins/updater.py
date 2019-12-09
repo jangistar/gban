01749c37c00b8a2004054b6a4299694dd1827bcc
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-12-09 15:29:53 +0530

    Update updater.py

--- stdplugins/updater.py
+++ stdplugins/updater.py
@@ -41,7 +41,7 @@ IS_SELECTED_DIFFERENT_BRANCH = (
     "in this case, Updater is unable to identify the branch to be updated."
     "please check out to an official branch, and re-start the updater."
 )
-OFFICIAL_UPSTREAM_REPO = "https://www.github.com/mkaraniya/bothub.got"
+OFFICIAL_UPSTREAM_REPO = "https://www.github.com/mkaraniya/bothub.git"
 BOT_IS_UP_TO_DATE = "the userbot is up-to-date."
 NEW_BOT_UP_DATE_FOUND = (
     "new update found for {branch_name}\n"
