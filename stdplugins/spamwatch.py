edc54c8bdbadf7369226a9fe341dbd71ae04d626
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-02-20 11:15:30 +0530

    Update spamwatch.py

--- stdplugins/spamwatch.py
+++ stdplugins/spamwatch.py
@@ -67,37 +67,37 @@ async def spamwatch_(event):
             )
 
 
-# async def get_user_from_event(event):
-#     if event.reply_to_msg_id:
-#         previous_message = await event.get_reply_message()
-#         user_obj = await event.client.get_entity(previous_message.from_id)
-#     else:
-#         user = event.pattern_match.group(1)
-#         if user.isnumeric():
-#             user = int(user)
-#         if not user:
-#             await event.edit("`Pass the user's username, id or reply!`")
-#             return
-#         if event.message.entities is not None:
-#             probable_user_mention_entity = event.message.entities[0]
+ async def get_user_from_event(event):
+     if event.reply_to_msg_id:
+         previous_message = await event.get_reply_message()
+         user_obj = await event.client.get_entity(previous_message.from_id)
+     else:
+         user = event.pattern_match.group(1)
+         if user.isnumeric():
+             user = int(user)
+         if not user:
+             await event.edit("`Pass the user's username, id or reply!`")
+             return
+         if event.message.entities is not None:
+             probable_user_mention_entity = event.message.entities[0]
 
-#             if isinstance(probable_user_mention_entity, MessageEntityMentionName):
-#                 user_id = probable_user_mention_entity.user_id
-#                 user_obj = await event.client.get_entity(user_id)
-#                 return user_obj
-#         try:
-#             user_obj = await event.client.get_entity(user)
-#         except (TypeError, ValueError) as err:
-#             await event.edit(str(err))
-#             return None
-#     return user_obj
+             if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                 user_id = probable_user_mention_entity.user_id
+                 user_obj = await event.client.get_entity(user_id)
+                 return user_obj
+         try:
+             user_obj = await event.client.get_entity(user)
+         except (TypeError, ValueError) as err:
+             await event.edit(str(err))
+             return None
+     return user_obj
 
-# async def get_user_from_id(user, event):
-#     if isinstance(user, str):
-#         user = int(user)
-#     try:
-#         user_obj = await event.client.get_entity(user)
-#     except (TypeError, ValueError) as err:
-#         await event.edit(str(err))
-#         return None
-#     return user_obj
+  async def get_user_from_id(user, event):
+     if isinstance(user, str):
+         user = int(user)
+     try:
+         user_obj = await event.client.get_entity(user)
+     except (TypeError, ValueError) as err:
+         await event.edit(str(err))
+         return None
+     return user_obj
