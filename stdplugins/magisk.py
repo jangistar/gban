73a8250c40ae473bd77ecad632e999509aa1d699
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-03-04 22:43:33 +0530

    Create magisk.py
    tried for Magisk module to work

---
+++ stdplugins/magisk.py
@@ -0,0 +1,34 @@
+""" Userbot module containing commands related to android"""
+
+import re
+from requests import get
+from bs4 import BeautifulSoup
+from uniborg.util import 
+
+
+
+GITHUB = 'https://github.com'
+DEVICES_DATA = 'https://raw.githubusercontent.com/androidtrackers/' \
+               'certified-android-devices/master/devices.json'
+
+
+@borg.on(admin_cmd(pattern='magisk(?: |$)(.*)'))
+async def magisk(request):
+    """ magisk latest releases """
+    magisk_dict = {
+        "Stable":
+        "https://raw.githubusercontent.com/topjohnwu/magisk_files/master/stable.json",
+        "Beta":
+        "https://raw.githubusercontent.com/topjohnwu/magisk_files/master/beta.json",
+        "Canary (Release)":
+        "https://raw.githubusercontent.com/topjohnwu/magisk_files/canary/release.json",
+        "Canary (Debug)":
+        "https://raw.githubusercontent.com/topjohnwu/magisk_files/canary/debug.json"
+    }
+    releases = 'Latest Magisk Releases:\n'
+    for name, release_url in magisk_dict.items():
+        data = get(release_url).json()
+        releases += f'{name}: [ZIP v{data["magisk"]["version"]}]({data["magisk"]["link"]}) | ' \
+                    f'[APK v{data["app"]["version"]}]({data["app"]["link"]}) | ' \
+                    f'[Uninstaller]({data["uninstaller"]["link"]})\n'
+    await request.edit(releases)
