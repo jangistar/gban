6593857bec6084dcc8a1191dbce275a476fac0e0
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-03-13 19:05:39 +0530

    Update botversion.py

--- stdplugins/botversion.py
+++ stdplugins/botversion.py
@@ -29,8 +29,8 @@ async def bot_ver(event):
             stderr=asyncPIPE,
         )
         stdout, stderr = await ver.communicate()
-        verout = +str(stdout.decode().strip()) \
-            + +str(stderr.decode().strip())
+        verout = str(stdout.decode().strip()) \
+            + str(stderr.decode().strip())
 
         invokerev = "git rev-list --all --count"
         rev = await asyncrunapp(
@@ -39,8 +39,8 @@ async def bot_ver(event):
             stderr=asyncPIPE,
         )
         stdout, stderr = await rev.communicate()
-        revout = +str(stdout.decode().strip()) \
-            + +str(stderr.decode().strip())
+        revout = str(stdout.decode().strip()) \
+            + str(stderr.decode().strip())
 
         await event.edit("`Userbot Version: "
                          f"{verout}"
