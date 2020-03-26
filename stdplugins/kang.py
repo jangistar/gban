1c6996f2b4c4839ec9fc863d2722e7c9d7e9d18f
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-03-26 20:13:14 +0530

    Update kang.py

--- stdplugins/kang.py
+++ stdplugins/kang.py
@@ -49,7 +49,7 @@ async def _(event):
     me = borg.me
     userid = event.from_id
     packname = f"{DEFAULTUSER}"
-    packshortname = f"{DEFAULTUSER}_{userid}"  # format: Uni_Borg_userid
+    packshortname = f"Bot_Hub_{userid}"  # format: Uni_Borg_userid
 
     is_a_s = is_it_animated_sticker(reply_message)
     file_ext_ns_ion = "BotHub_Sticker.png"
@@ -59,7 +59,7 @@ async def _(event):
         file_ext_ns_ion = "BotHub_AnimatedSticker.tgs"
         uploaded_sticker = await borg.upload_file(file, file_name=file_ext_ns_ion)
         packname = f"{DEFAULTUSER}_AnimatedStickers"
-        packshortname = f"{DEFAULTUSER}_Animated"  # format: Uni_Borg_userid
+        packshortname = f"Bot_Hub_{userid}"  # format: Uni_Borg_userid
     elif not is_message_image(reply_message):
         await event.edit("Invalid message type")
         return
