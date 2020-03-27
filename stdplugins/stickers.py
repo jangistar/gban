0c062159aab6dd79d2795811d7a3f4376a8e7ec4
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-03-27 16:20:54 +0530

    Update stickers.py

--- stdplugins/stickers.py
+++ stdplugins/stickers.py
@@ -46,7 +46,7 @@ KANGING_STR = [
 async def _(event):
 #async def copi(args):
     """ For .copi command, kangs stickers or creates new ones. """
-    user = await user.username.get_me()
+     user = await tgbot.get_me()
     if not user.username:
         user.username = user.first_name
     message = await args.get_reply_message()
@@ -59,18 +59,18 @@ async def _(event):
         if isinstance(message.media, MessageMediaPhoto):
             await args.edit(f"`{random.choice(KANGING_STR)}`")
             photo = io.BytesIO()
-            photo = await user.username.download_media(message.photo, photo)
+            photo = await tgbot.download_media(message.photo, photo)
         elif "image" in message.media.document.mime_type.split('/'):
             await args.edit(f"`{random.choice(KANGING_STR)}`")
             photo = io.BytesIO()
-            await user.username.download_file(message.media.document, photo)
+            await tgbot.download_file(message.media.document, photo)
             if (DocumentAttributeFilename(file_name='sticker.webp') in
                     message.media.document.attributes):
                 emoji = message.media.document.attributes[1].alt
                 emojibypass = True
         elif "tgsticker" in message.media.document.mime_type:
             await args.edit(f"`{random.choice(KANGING_STR)}`")
-            await user.username.download_file(message.media.document,
+            await tgbot.download_file(message.media.document,
                                     'AnimatedSticker.tgs')
 
             attributes = message.media.document.attributes
@@ -125,11 +125,11 @@ async def _(event):
         htmlstr = response.read().decode("utf8").split('\n')
 
         if "  A <strong>Telegram</strong> user has created the <strong>Sticker&nbsp;Set</strong>." not in htmlstr:
-            async with user.username.conversation('Stickers') as conv:
+            async with tgbot.conversation('Stickers') as conv:
                 await conv.send_message('/addsticker')
                 await conv.get_response()
                 # Ensure user doesn't get spamming notifications
-                await user.username.send_read_acknowledge(conv.chat_id)
+                await tgbot.send_read_acknowledge(conv.chat_id)
                 await conv.send_message(packname)
                 x = await conv.get_response()
                 while "120" in x.text:
@@ -144,11 +144,11 @@ async def _(event):
                         await conv.send_message(cmd)
                         await conv.get_response()
                         # Ensure user doesn't get spamming notifications
-                        await user.username.send_read_acknowledge(conv.chat_id)
+                        await tgbot.send_read_acknowledge(conv.chat_id)
                         await conv.send_message(packnick)
                         await conv.get_response()
                         # Ensure user doesn't get spamming notifications
-                        await user.username.send_read_acknowledge(conv.chat_id)
+                        await tgbot.send_read_acknowledge(conv.chat_id)
                         if is_anim:
                             await conv.send_file('AnimatedSticker.tgs')
                             remove('AnimatedSticker.tgs')
@@ -158,7 +158,7 @@ async def _(event):
                         await conv.get_response()
                         await conv.send_message(emoji)
                         # Ensure user doesn't get spamming notifications
-                        await user.username.send_read_acknowledge(conv.chat_id)
+                        await tgbot.send_read_acknowledge(conv.chat_id)
                         await conv.get_response()
                         await conv.send_message("/publish")
                         if is_anim:
@@ -166,17 +166,17 @@ async def _(event):
                             await conv.send_message(f"<{packnick}>")
                         # Ensure user doesn't get spamming notifications
                         await conv.get_response()
-                        await user.username.send_read_acknowledge(conv.chat_id)
+                        await tgbot.send_read_acknowledge(conv.chat_id)
                         await conv.send_message("/skip")
                         # Ensure user doesn't get spamming notifications
-                        await user.username.send_read_acknowledge(conv.chat_id)
+                        await tgbot.send_read_acknowledge(conv.chat_id)
                         await conv.get_response()
                         await conv.send_message(packname)
                         # Ensure user doesn't get spamming notifications
-                        await user.username.send_read_acknowledge(conv.chat_id)
+                        await tgbot.send_read_acknowledge(conv.chat_id)
                         await conv.get_response()
                         # Ensure user doesn't get spamming notifications
-                        await user.username.send_read_acknowledge(conv.chat_id)
+                        await tgbot.send_read_acknowledge(conv.chat_id)
                         await args.edit(f"`Sticker added in a Different Pack !\
                             \nThis Pack is Newly created!\
                             \nYour pack can be found [here](t.me/addstickers/{packname})",
@@ -196,23 +196,23 @@ async def _(event):
                     return
                 await conv.send_message(emoji)
                 # Ensure user doesn't get spamming notifications
-                await user.username.send_read_acknowledge(conv.chat_id)
+                await tgbot.send_read_acknowledge(conv.chat_id)
                 await conv.get_response()
                 await conv.send_message('/done')
                 await conv.get_response()
                 # Ensure user doesn't get spamming notifications
-                await user.username.send_read_acknowledge(conv.chat_id)
+                await tgbot.send_read_acknowledge(conv.chat_id)
         else:
             await args.edit("`Brewing a new Pack...`")
-            async with user.username.conversation('Stickers') as conv:
+            async with tgbot.conversation('Stickers') as conv:
                 await conv.send_message(cmd)
                 await conv.get_response()
                 # Ensure user doesn't get spamming notifications
-                await user.username.send_read_acknowledge(conv.chat_id)
+                await tgbot.send_read_acknowledge(conv.chat_id)
                 await conv.send_message(packnick)
                 await conv.get_response()
                 # Ensure user doesn't get spamming notifications
-                await user.username.send_read_acknowledge(conv.chat_id)
+                await tgbot.send_read_acknowledge(conv.chat_id)
                 if is_anim:
                     await conv.send_file('AnimatedSticker.tgs')
                     remove('AnimatedSticker.tgs')
@@ -227,7 +227,7 @@ async def _(event):
                     return
                 await conv.send_message(emoji)
                 # Ensure user doesn't get spamming notifications
-                await user.username.send_read_acknowledge(conv.chat_id)
+                await tgbot.send_read_acknowledge(conv.chat_id)
                 await conv.get_response()
                 await conv.send_message("/publish")
                 if is_anim:
@@ -235,17 +235,17 @@ async def _(event):
                     await conv.send_message(f"<{packnick}>")
                 # Ensure user doesn't get spamming notifications
                 await conv.get_response()
-                await user.username.send_read_acknowledge(conv.chat_id)
+                await tgbot.send_read_acknowledge(conv.chat_id)
                 await conv.send_message("/skip")
                 # Ensure user doesn't get spamming notifications
-                await user.username.send_read_acknowledge(conv.chat_id)
+                await tgbot.send_read_acknowledge(conv.chat_id)
                 await conv.get_response()
                 await conv.send_message(packname)
                 # Ensure user doesn't get spamming notifications
-                await user.username.send_read_acknowledge(conv.chat_id)
+                await tgbot.send_read_acknowledge(conv.chat_id)
                 await conv.get_response()
                 # Ensure user doesn't get spamming notifications
-                await user.username.send_read_acknowledge(conv.chat_id)
+                await tgbot.send_read_acknowledge(conv.chat_id)
 
         await args.edit(f"`Sticker kanged successfully!`\
             \nPack can be found [here](t.me/addstickers/{packname})",
@@ -275,6 +275,4 @@ async def resize_photo(photo):
         image.thumbnail(maxsize)
 
     return image
-
-
 
