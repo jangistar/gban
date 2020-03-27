bc6a2870115088dc5271f2853b9361f27f4eeff0
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-03-27 23:47:23 +0530

    Update heroku.py

--- stdplugins/heroku.py
+++ stdplugins/heroku.py
@@ -4,6 +4,7 @@
 from telethon import events
 from datetime import datetime
 from uniborg.util import admin_cmd
+import subprocess
 import importlib.util
 import asyncio
 import random
@@ -33,6 +34,46 @@ import git
 import heroku3
 import json
 from sample_config import Config
+from datetime import datetime
+from random import randint
+from asyncio import sleep
+from os import execl
+import sys
+import os
+import io
+import heroku3
+import asyncio
+from asyncio import create_subprocess_shell as asyncSubprocess
+from asyncio.subprocess import PIPE as asyncPIPE
+import sys
+import json
+from speedtest import Speedtest
+from telethon import functions
+from os import remove, execle, path, makedirs, getenv, environ
+from shutil import rmtree
+import asyncio
+import json
+from asyncio import sleep
+from telethon.errors import rpcbaseerrors
+import os
+import subprocess
+import time
+import math
+from pySmartDL import SmartDL
+import asyncio
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+from telethon.tl.types import DocumentAttributeVideo
+import sys
+from git import Repo
+from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
+from asyncio import create_subprocess_shell as asyncrunapp
+from asyncio.subprocess import PIPE as asyncPIPE
+from platform import python_version, uname
+from shutil import which
+from os import remove
+from telethon import version
+
 
 # ================= CONSTANT =================
 HEROKU_API_KEY = Config.HEROKU_API_KEY
