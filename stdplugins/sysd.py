bf7ff2886d8f78270b9c444d67bc369d181e211f
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-03-29 16:08:51 +0530

    Update sysd.py

--- stdplugins/sysd.py
+++ stdplugins/sysd.py
@@ -3,10 +3,12 @@ from asyncio import create_subprocess_shell as asyncrunapp
 from asyncio.subprocess import PIPE as asyncPIPE
 from platform import python_version, uname
 from shutil import which
-
+from uniborg.util import admin_cmd
 from telethon import events
 import asyncio
 from collections import deque
+from os import remove
+from telethon import version
 
 
 @borg.on(events.NewMessage(pattern=r"\.sysd", outgoing=True))
@@ -32,3 +34,22 @@ async def sysdetails(sysd):
         except FileNotFoundError:
             await sysd.edit("`Hey, on mkaraniya/BotHub install .neofetch first kthx`")
 
+
+@borg.on(admin_cmd(pattern=r"\.sysd", outgoing=True))
+async def sysdetails(sysd):
+    """ For .sysd command, get system info using neofetch. """
+    try:
+        neo = "neofetch --stdout"
+        fetch = await asyncrunapp(
+            neo,
+            stdout=asyncPIPE,
+            stderr=asyncPIPE,
+        )
+
+        stdout, stderr = await fetch.communicate()
+        result = str(stdout.decode().strip()) \
+            + str(stderr.decode().strip())
+
+        await sysd.edit("`" + result + "`")
+    except FileNotFoundError:
+        await sysd.edit("`Install neofetch first !!`")
