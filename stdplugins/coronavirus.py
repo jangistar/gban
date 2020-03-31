6068252163deda065f31eba5116ba7de3a108337
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-04-01 01:41:42 +0530

    Update coronavirus.py

--- stdplugins/coronavirus.py
+++ stdplugins/coronavirus.py
@@ -16,6 +16,6 @@ async def _(event):
 
 def get_country_data(country, world):
     for country_data in world:
-        if country_data["country"].lower() == country.lower():
+        if country_data["country"] == country:
             return country_data
     return {"Status": "No information yet about this country!"}
