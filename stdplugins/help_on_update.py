df47c719e3a0fe02e42a44e8984c2a5786bcc681
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-04-01 21:37:08 +0530

    Update help_on_update.py

--- stdplugins/help_on_update.py
+++ stdplugins/help_on_update.py
@@ -38,10 +38,10 @@ DEFAULTUSER = Config.ALIVE_NAME if Config.ALIVE_NAME else uname().node
 
 
 @borg.on(admin_cmd(pattern="useit (.*)"))
-async def _(event):
-#async def usit(e):
-    await event.edit(
-        "Here's something for you to useit for help_on_update:\n"
+#async def _(event):
+async def usit(e):
+    await e.edit(
+        f"Here's something for {DEFAULTUSER} to use it for help_on_update:\n"
         "\n[Windows Method](https://telegra.ph/How-to-keep-BotHub-repo-updated-while-keeping-your-changes-through-windows-cmd-method-04-01)"
         "\n[Termux Method](https://telegra.ph/How-to-keep-BotHub-repo-updated-while-keeping-your-changes-through-windows-cmd-method-04-01-2)"
         "\n[Kali Linux Method](https://telegra.ph/How-to-keep-BotHub-repo-updated-while-keeping-your-changes-through-windows-cmd-method-04-01-2)"
