29370213cae387b0b85dd6693ecd5a142fa68c69
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-04-06 01:44:30 +0530

    Create global_ban.py

---
+++ stdplugins/global_ban.py
@@ -0,0 +1,208 @@
+""".gban , .ungban"""
+
+from asyncio import sleep
+import requests
+from os import remove
+from telethon import events, errors, functions, types
+from telethon.tl import functions, types
+from platform import python_version, uname
+from telethon.errors import (BadRequestError, ChatAdminRequiredError,
+                             ImageProcessFailedError, PhotoCropSizeSmallError,
+                             UserAdminInvalidError)
+from telethon.errors.rpcerrorlist import (UserIdInvalidError,
+                                          MessageTooLongError)
+from telethon.tl.functions.channels import (EditAdminRequest,
+                                            EditBannedRequest,
+                                            EditPhotoRequest)
+from telethon.tl.functions.messages import UpdatePinnedMessageRequest
+from telethon.tl.types import (PeerChannel, ChannelParticipantsAdmins,
+                               ChatAdminRights, ChatBannedRights,
+                               MessageEntityMentionName, MessageMediaPhoto,
+                               ChannelParticipantsBots)
+
+from telethon.tl.functions.users import GetFullUserRequest
+
+#from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, bot
+#from userbot.events import register
+
+from telethon import events
+import logging
+import asyncio
+from uniborg.util import admin_cmd
+from telethon.tl import functions, types
+from uniborg.util import admin_cmd
+from platform import python_version, uname
+from sample_config import Config
+import io
+import re
+
+
+# ================= CONSTANT =================
+BOTLOG = Config.BOTLOG
+BOTLOG_CHATID = Config.PRIVATE_GROUP_BOT_API_ID
+# ================= CONSTANT =================
+
+# =================== CONSTANT ===================
+PP_TOO_SMOL = "`The image is too small`"
+PP_ERROR = "`Failure while processing the image`"
+NO_ADMIN = "`I am not an admin!`"
+NO_PERM = "`I don't have sufficient permissions!`"
+NO_SQL = "`Running on Non-SQL mode!`"
+
+CHAT_PP_CHANGED = "`Chat Picture Changed`"
+CHAT_PP_ERROR = "`Some issue with updating the pic,`" \
+                "`maybe coz I'm not an admin,`" \
+                "`or don't have enough rights.`"
+INVALID_MEDIA = "`Invalid Extension`"
+
+
+BANNED_RIGHTS = ChatBannedRights(
+    until_date=None,
+    view_messages=True,
+    send_messages=True,
+    send_media=True,
+    send_stickers=True,
+    send_gifs=True,
+    send_games=True,
+    send_inline=True,
+    embed_links=True,
+)
+
+UNBAN_RIGHTS = ChatBannedRights(
+    until_date=None,
+    send_messages=None,
+    send_media=None,
+    send_stickers=None,
+    send_gifs=None,
+    send_games=None,
+    send_inline=None,
+    embed_links=None,
+)
+
+MUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=True)
+
+UNMUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=False)
+# ================================================
+
+
+@borg.on(events.NewMessage(incoming=True))
+async def on_new_message(event):
+#async def muter(moot):
+    """ Used for deleting the messages of muted people """
+    try:
+        from userbot.modules.sql_helper.spam_mute_sql import is_muted
+        from userbot.modules.sql_helper.gmute_sql import is_gmuted
+    except AttributeError:
+        return
+    muted = is_muted(moot.chat_id)
+    gmuted = is_gmuted(moot.sender_id)
+    rights = ChatBannedRights(
+        until_date=None,
+        send_messages=True,
+        send_media=True,
+        send_stickers=True,
+        send_gifs=True,
+        send_games=True,
+        send_inline=True,
+        embed_links=True,
+    )
+    if muted:
+        for i in muted:
+            if str(i.sender) == str(moot.sender_id):
+                await moot.delete()
+                await moot.client(
+                    EditBannedRequest(moot.chat_id, moot.sender_id, rights))
+    for i in gmuted:
+        if i.sender == str(moot.sender_id):
+            await moot.delete()
+
+
+#@register(outgoing=True, pattern="^.ungmute(?: |$)(.*)")
+#async def ungmoot(un_gmute):
+@borg.on(events.NewMessage(pattern=r"\.ungban", outgoing=True))
+async def _(event):
+    """ For .ungmute command, ungmutes the target in the userbot """
+    # Admin or creator check
+    chat = await un_gmute.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+
+    # If not admin and not creator, return
+    if not admin and not creator:
+        await un_gmute.edit(NO_ADMIN)
+        return
+
+    # Check if the function running under SQL mode
+    try:
+        from userbot.modules.sql_helper.gmute_sql import ungmute
+    except AttributeError:
+        await un_gmute.edit(NO_SQL)
+        return
+
+    user = await get_user_from_event(un_gmute)
+    user = user[0]
+    if user:
+        pass
+    else:
+        return
+
+    # If pass, inform and start ungmuting
+    await un_gmute.edit('```Ungmuting...```')
+
+    if ungmute(user.id) is False:
+        await un_gmute.edit("`Error! User probably not gmuted.`")
+    else:
+        # Inform about success
+        await un_gmute.edit("```Ungmuted Successfully```")
+
+        if BOTLOG:
+            await un_gmute.client.send_message(
+                BOTLOG_CHATID, "#UNGMUTE\n"
+                f"USER: [{user.first_name}](tg://user?id={user.id})\n"
+                f"CHAT: {un_gmute.chat.title}(`{un_gmute.chat_id}`)")
+
+
+#@register(outgoing=True, pattern="^.gmute(?: |$)(.*)")
+#async def gspider(gspdr):
+@borg.on(events.NewMessage(pattern=r"\.gban", outgoing=True))
+async def _(event):
+    """ For .gmute command, globally mutes the replied/tagged person """
+    # Admin or creator check
+    chat = await gspdr.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+
+    # If not admin and not creator, return
+    if not admin and not creator:
+        await gspdr.edit(NO_ADMIN)
+        return
+
+    # Check if the function running under SQL mode
+    try:
+        from userbot.modules.sql_helper.gmute_sql import gmute
+    except AttributeError:
+        await gspdr.edit(NO_SQL)
+        return
+
+    user, reason = await get_user_from_event(gspdr)
+    if user:
+        pass
+    else:
+        return
+
+    # If pass, inform and start gmuting
+    await gspdr.edit("`Grabs a huge, sticky duct tape!`")
+    if gmute(user.id) is False:
+        await gspdr.edit(
+            '`Error! User probably already gmuted.\nRe-rolls the tape.`')
+    else:
+        if reason:
+            await gspdr.edit(f"`Globally taped!`Reason: {reason}")
+        else:
+            await gspdr.edit("`Globally taped!`")
+
+        if BOTLOG:
+            await gspdr.client.send_message(
+                BOTLOG_CHATID, "#GMUTE\n"
+                f"USER: [{user.first_name}](tg://user?id={user.id})\n"
+                f"CHAT: {gspdr.chat.title}(`{gspdr.chat_id}`)")
