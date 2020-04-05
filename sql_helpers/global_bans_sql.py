3d39d98c6aab128f4d0f2a95b92b36ddc92c00ab
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-04-05 18:29:44 +0530

    Update global_bans_sql.py

--- sql_helpers/global_bans_sql.py
+++ sql_helpers/global_bans_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Integer, String, Boolean
 
-from sql_helppers import BASE, SESSION
+from sql_helpers import BASE, SESSION
 
 
 class GloballyBannedUsers(BASE):
