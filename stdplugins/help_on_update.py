a36a290409d87a8a8cbacc00208de5d4cf815eae
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-04-05 22:04:32 +0530

    Update help_on_update.py

--- stdplugins/help_on_update.py
+++ stdplugins/help_on_update.py
@@ -55,20 +55,14 @@ async def usit(e):
         "\n[Ubuntu Linux Method](https://telegra.ph/How-to-keep-BotHub-repo-updated-while-keeping-your-changes-through-Ubuntu-Terminal-method-04-01)"
         "\n[Special - Note](https://telegra.ph/Special-Note-11-02)")
     
-@borg.on(events.NewMessage(pattern=r"\.puseit", outgoing=True))
-async def pusit(f):
-      builder = event.builder
-      result = builder.article(
-    await f.edit(
-        text="""Here's something for {DEFAULTUSER} to use it for help_on_update on **BotHub**""",
-            buttons=[
-                    [custom.Button.url("👤Contact Creator👤", "https://telegram.dog/Three_Cube_TeKnoways"), custom.Button.url(
-                        "🎞My YouTube Channel🎞", "https://www.youtube.com/channel/UCeKQxQK7XZ3jGi3541uWATg?sub_confirmation=1")],
-                    [custom.Button.url("🎛Source Code🎛", "https://github.com/mkaraniya/BotHub"), custom.Button.url(
-                        "❕❗Deploy Me❗❕", "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2Fmkaraniya%2FBotHub%2F&template=https%3A%2F%2Fgithub.com%2Fmkaraniya%2FBotHub%2F")],
-                    [custom.Button.url("🔰Update Fork🔰", "tg://need_update_for_some_feature"), custom.Button.url(
-                        "✳️Fork Boost✳️", "tg://some_unsupported_feature"), custom.Button.url(
-                        "♻️Refresh Heroku♻️", "tg://some_unsupported_feature")]
-               
-                ],
-                link_preview=True))
+    
+    
+@borg.on(events.NewMessage(pattern=r"\.pushit", outgoing=True))
+async def pushit(e):
+    await e.edit(
+        f"Here's something for {DEFAULTUSER} to use it for help_on_update on **BotHub**:\n"
+        "\n[Windows Method](buttonurl:https://telegra.ph/How-to-keep-BotHub-repo-updated-while-keeping-your-changes-through-windows-cmd-method-04-01)"
+        "\n[Termux Method](buttonurl:https://telegra.ph/How-to-keep-BotHub-repo-updated-while-keeping-your-changes-through-windows-cmd-method-04-01-2)"
+        "\n[Kali Linux Method](buttonurl:https://telegra.ph/How-to-keep-BotHub-repo-updated-while-keeping-your-changes-through-windows-cmd-method-04-01-2)"
+        "\n[Ubuntu Linux Method](buttonurl:https://telegra.ph/How-to-keep-BotHub-repo-updated-while-keeping-your-changes-through-Ubuntu-Terminal-method-04-01)"
+        "\n[Special - Note](buttonurl:https://telegra.ph/Special-Note-11-02)")
