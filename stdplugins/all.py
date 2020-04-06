42b5c82e81b482b1284ca90edecc2dfe540e15b1
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-04-06 20:10:38 +0530

    Update all.py

--- stdplugins/all.py
+++ stdplugins/all.py
@@ -4,14 +4,15 @@
 """A Plugin to tagall in the chat for @UniBorg and cmd is `.all`"""
 
 from telethon import events
+from uniborg.util import admin_cmd
 
-
-@borg.on(events.NewMessage(pattern=r"\.all", outgoing=True))
+# @borg.on(events.NewMessage(pattern=r"\.all", outgoing=True))
+@borg.on(admin_cmd("all"))
 async def _(event):
     if event.fwd_from:
         return
     await event.delete()
-    mentions = "@all"
+    mentions = "@SAB"
     chat = await event.get_input_chat()
     async for x in borg.iter_participants(chat, 100):
         mentions += f"[\u2063](tg://user?id={x.id})"
