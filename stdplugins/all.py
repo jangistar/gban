a3ba494f701efa4908654f6fa8bc92ba031f5c01
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-04-06 21:00:23 +0530

    Update all.py

--- stdplugins/all.py
+++ stdplugins/all.py
@@ -16,4 +16,4 @@ async def _(event):
     chat = await event.get_input_chat()
     async for x in borg.iter_participants(chat, 100):
         mentions += f"[\u2063](tg://user?id={x.id})"
-    await borg.send_message(mentions)
+    await borg.send_message(chat, mentions, reply_to=event.message.reply_to_msg_id)
