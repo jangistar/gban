a1a80d0594e7dbc4b07839c959a61ebb1db7ca5a
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-04-10 03:29:52 +0530

    Update and rename gban_ungban to gban_ungban.py
    supposedly fix

--- stdplugins/gban_ungban.py
+++ stdplugins/gban_ungban.py
@@ -7,11 +7,9 @@ import asyncio
 from uniborg.util import admin_cmd
 from sample_config import Config
 
-@borg.on(admin_cmd(pattern="gban ?(.*)", allow_sudo=True))
+#@borg.on(admin_cmd(pattern="gban ?(.*)", allow_sudo=True))
+@borg.on(admin_cmd("gban ?(.*)"))
 async def _(event):
-    if Config.G_BAN_LOGGER_GROUP is None:
-        await event.edit("ENV VAR is not set. This module will not work.")
-        return
     if event.fwd_from:
         return
     reason = event.pattern_match.group(1)
@@ -23,17 +21,14 @@ async def _(event):
             r_from_id = r.from_id
         await borg.send_message(
             Config.G_BAN_LOGGER_GROUP,
-            ".ban 🤮 [{}](tg://user?id={}) `{}`".format(r_from_id, reason)
-        )
-    await asyncio.sleep(5)
+            "!gban 🤮 [user](tg://user?id={}) {}".format(r_from_id, reason)
+       )
     await event.delete()
 
 
-@borg.on(admin_cmd(pattern="ungban ?(.*)", allow_sudo=True))
+#@borg.on(admin_cmd(pattern="ungban ?(.*)", allow_sudo=True))
+@borg.on(admin_cmd("ungban ?(.*)"))
 async def _(event):
-    if Config.G_BAN_LOGGER_GROUP is None:
-        await event.edit("ENV VAR is not set. This module will not work.")
-        return
     if event.fwd_from:
         return
     reason = event.pattern_match.group(1)
@@ -42,7 +37,6 @@ async def _(event):
         r_from_id = r.from_id
         await borg.send_message(
             Config.G_BAN_LOGGER_GROUP,
-            ".unban 🤠 [{}](tg://user?id={}) `{}`".format(r_from_id, reason)
+            "!ungban 🤠 [user](tg://user?id={}) {}".format(r_from_id, reason)
         )
-    await asyncio.sleep(5)
     await event.delete()
