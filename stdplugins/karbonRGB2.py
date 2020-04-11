02113d66fd0a5f378152510ed46368c28f46684f
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-04-11 14:20:19 +0530

    Update karbonRGB2.py

--- stdplugins/karbonRGB2.py
+++ stdplugins/karbonRGB2.py
@@ -34,7 +34,7 @@ async def carbon_api(e):
    url = CARBON.format(code=code, R=RED, G=GREEN, B=BLUE, O=OPC, lang=CARBONLANG)
    chrome_options = Options()
    chrome_options.add_argument("--headless")
-   chrome_options.binary_location = Config.GOOGLE_CHROME_BIN
+   chrome_options.binary_location = Config.CARBON_BIN
    chrome_options.add_argument("--window-size=1920x1080")
    chrome_options.add_argument("--disable-dev-shm-usage")
    chrome_options.add_argument("--no-sandbox")
@@ -43,7 +43,7 @@ async def carbon_api(e):
    chrome_options.add_experimental_option('prefs', prefs)
    await e.edit("⬛⬛⬜⬜⬜")
 
-   driver = webdriver.Chrome(executable_path=Config.CHROME_DRIVER, options=chrome_options)
+   driver = webdriver.Chrome(executable_path=Config.CARBON_DRIVER, options=chrome_options)
    driver.get(url)
    download_path = './'
    driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
