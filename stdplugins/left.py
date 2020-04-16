66ef72b00e943fd04e20404ad4f9b3050eb029e4
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-04-16 17:05:56 +0530

    Update left.py

--- stdplugins/left.py
+++ stdplugins/left.py
@@ -6,7 +6,7 @@ from telethon.tl.types import ChannelParticipantsAdmins
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("left"))
+@borg.on(admin_cmd(pattern="left ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
