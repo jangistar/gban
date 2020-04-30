001c9dab1b38ce9b7d3d70bcae3d71b12560be71
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-05-01 04:36:29 +0530

    Create gban.py

---
+++ stdplugins/gban.py
@@ -0,0 +1,108 @@
+"""Restrict Users
+Available Commands: .gban, .ungban, .gmute, .ungmute
+.unmute added by @Mayur_Karaniya."""
+from telethon import events, functions, types
+import asyncio
+from datetime import datetime
+from telethon.tl.functions.channels import EditBannedRequest
+from telethon.tl.types import ChatBannedRights
+from uniborg.util import admin_cmd
+from sample_config import Config
+#from sql_helpers.gban_sql import listmychatids
+
+# chat_ids = Config.CHAT_IDS
+
+#@borg.on(events.NewMessage(pattern=r"\-listmychatids", outgoing=True))
+async def listmychatids(chat_ids):
+    if chat_ids.fwd_from:
+        return
+    result = await borg(functions.channels.GetAdminedPublicChannelsRequest())
+    output_str = ""
+    for channel_obj in result.chats:
+        output_str += f"{channel_obj.id} \n"
+    await chat_ids.edit(output_str)
+    return channel_obj
+    
+    
+    
+
+unbanned_rights = ChatBannedRights(
+    until_date=None,
+    view_messages=None,
+    send_messages=None,
+    send_media=None,
+    send_stickers=None,
+    send_gifs=None,
+    send_games=None,
+    send_inline=None,
+    embed_links=None
+)
+muted_rights = ChatBannedRights(
+    until_date=None,
+    view_messages=None,
+    send_messages=True,
+    send_media=True,
+    send_stickers=True,
+    send_gifs=True,
+    send_games=True,
+    send_inline=True,
+    embed_links=True
+)
+unmuted_rights = ChatBannedRights(
+    until_date=None,
+    view_messages=None,
+    send_messages=None,
+    send_media=None,
+    send_stickers=None,
+    send_gifs=None,
+    send_games=None,
+    send_inline=None,
+    embed_links=None
+)
+banned_rights = ChatBannedRights(
+    until_date=None,
+    view_messages=True,
+    send_messages=True,
+    send_media=True,
+    send_stickers=True,
+    send_gifs=True,
+    send_games=True,
+    send_inline=True,
+    embed_links=True
+)
+
+
+
+@borg.on(admin_cmd("(gban|ungban|gmute|ungmute) ?(.*)"))
+async def _(event):
+    # Space weirdness in regex required because argument is optional and other
+    # commands start with ".unban"
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    to_ban_id = None
+    rights = None
+    input_cmd = event.pattern_match.group(1)
+    if input_cmd == "gban":
+        rights = banned_rights
+    elif input_cmd == "ungban":
+        rights = unbanned_rights
+    elif input_cmd == "gmute":
+        rights = muted_rights
+    elif input_cmd == "ungmute":
+        rights = unmuted_rights
+    input_str = event.pattern_match.group(2)
+    reply_msg_id = event.reply_to_msg_id
+    if reply_msg_id:
+        r_mesg = await event.get_reply_message()
+        to_ban_id = r_mesg.from_id
+    elif input_str and "all" not in input_str:
+        to_ban_id = int(input_str)
+    else:
+        return False
+    try:
+        await borg(EditBannedRequest(listmychatids.chat_ids, to_ban_id, rights))
+    except (Exception) as exc:
+        await event.edit(str(exc))
+    else:
+        await event.edit(f"{input_cmd}ed Successfully")
