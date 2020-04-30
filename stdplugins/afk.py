3b808bc08b1a48ea3d02668b2a873f890590da9a
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-04-30 15:42:28 +0530

    Update afk.py

--- stdplugins/afk.py
+++ stdplugins/afk.py
@@ -51,15 +51,15 @@ AFKSTR = [
 ]
 # ============================================
 
-global USER_AFKB  # pylint:disable=E0602
-global afkb_time  # pylint:disable=E0602
-global last_afkb_message  # pylint:disable=E0602
-global afkb_start
-global afkb_end
-USER_AFKB = {}
-afkb_time = None
-last_afkb_message = {}
-afkb_start = {}
+global USER_AFK  # pylint:disable=E0602
+global afk_time  # pylint:disable=E0602
+global last_afk_message  # pylint:disable=E0602
+global afk_start
+global afk_end
+USER_AFK = {}
+afk_time = None
+last_afk_message = {}
+afk_start = {}
 
 AFKSK = str(choice(AFKSTR))
 
@@ -67,28 +67,28 @@ AFKSK = str(choice(AFKSTR))
 async def _(event):
     if event.fwd_from:
         return
-    global USER_AFKB  # pylint:disable=E0602
-    global afkb_time  # pylint:disable=E0602
-    global last_afkb_message  # pylint:disable=E0602
-    global afkb_start
-    global afkb_end
+    global USER_AFK  # pylint:disable=E0602
+    global afk_time  # pylint:disable=E0602
+    global last_afk_message  # pylint:disable=E0602
+    global afk_start
+    global afk_end
     global reason
-    USER_AFKB = {}
-    afkb_time = None
-    last_afkb_message = {}
-    afkb_end = {}
+    USER_AFK = {}
+    afk_time = None
+    last_afk_message = {}
+    afk_end = {}
     start_1 = datetime.now()
-    afkb_start = start_1.replace(microsecond=0)
+    afk_start = start_1.replace(microsecond=0)
     reason = event.pattern_match.group(1)
-    if not USER_AFKB:  # pylint:disable=E0602
+    if not USER_AFK:  # pylint:disable=E0602
         last_seen_status = await borg(  # pylint:disable=E0602
             functions.account.GetPrivacyRequest(
                 types.InputPrivacyKeyStatusTimestamp()
             )
         )
         if isinstance(last_seen_status.rules, types.PrivacyValueAllowAll):
-            afkb_time = datetime.datetime.now()  # pylint:disable=E0602
-        USER_AFKB = f"yes: {reason}"  # pylint:disable=E0602
+            afk_time = datetime.datetime.now()  # pylint:disable=E0602
+        USER_AFK = f"yes: {reason}"  # pylint:disable=E0602
         if reason:
             await borg.send_message(event.chat_id, f"**My King 👑 {DEFAULTUSER} 👑 is Going afk!** __because My Master is {reason}__")
         else:
@@ -105,18 +105,18 @@ async def _(event):
 
 
 @borg.on(events.NewMessage(outgoing=True))  # pylint:disable=E0602
-async def set_not_afkb(event):
-    global USER_AFKB  # pylint:disable=E0602
-    global afkb_time  # pylint:disable=E0602
-    global last_afkb_message  # pylint:disable=E0602
-    global afkb_start
-    global afkb_end
+async def set_not_afk(event):
+    global USER_AFK  # pylint:disable=E0602
+    global afk_time  # pylint:disable=E0602
+    global last_afk_message  # pylint:disable=E0602
+    global afk_start
+    global afk_end
     back_alive = datetime.now()
-    afkb_end = back_alive.replace(microsecond=0)
-    total_afkb_time = str(afkb_end - afkb_start)
+    afk_end = back_alive.replace(microsecond=0)
+    total_afk_time = str(afk_end - afk_start)
     current_message = event.message.message
-    if ".afkb" not in current_message and "yes" in USER_AFKB:  # pylint:disable=E0602
-        shite = await borg.send_message(event.chat_id, "__My Master is Back!__\n**He is No Longer afk.**\n `Was afk for:``" + total_afkb_time + "`")
+    if ".afk" not in current_message and "yes" in USER_AFK:  # pylint:disable=E0602
+        shite = await borg.send_message(event.chat_id, "__My Master is Back!__\n**He is No Longer afk.**\n `Was afk for:``" + total_afk_time + "`")
         try:
             await borg.send_message(  # pylint:disable=E0602
                 Config.PRIVATE_GROUP_BOT_API_ID,  # pylint:disable=E0602
@@ -133,36 +133,36 @@ async def set_not_afkb(event):
             )
         await asyncio.sleep(5)
         await shite.delete()
-        USER_AFKB = {}  # pylint:disable=E0602
-        afkb_time = None  # pylint:disable=E0602
+        USER_AFK = {}  # pylint:disable=E0602
+        afk_time = None  # pylint:disable=E0602
 
 
 @borg.on(events.NewMessage(  # pylint:disable=E0602
     incoming=True,
     func=lambda e: bool(e.mentioned or e.is_private)
 ))
-async def on_afkb(event):
+async def on_afk(event):
     if event.fwd_from:
         return
-    global USER_AFKB  # pylint:disable=E0602
-    global afkb_time  # pylint:disable=E0602
-    global last_afkb_message  # pylint:disable=E0602
-    global afkb_start
-    global afkb_end
+    global USER_AFK  # pylint:disable=E0602
+    global afk_time  # pylint:disable=E0602
+    global last_afk_message  # pylint:disable=E0602
+    global afk_start
+    global afk_end
     back_alivee = datetime.now()
-    afkb_end = back_alivee.replace(microsecond=0)
-    total_afkb_time = str(afkb_end - afkb_start)
-    afkb_since = "**a while ago**"
+    afk_end = back_alivee.replace(microsecond=0)
+    total_afk_time = str(afk_end - afk_start)
+    afk_since = "**a while ago**"
     current_message_text = event.message.message.lower()
-    if "afkb" in current_message_text:
+    if "afk" in current_message_text:
         # userbot's should not reply to other userbot's
         # https://core.telegram.org/bots/faq#why-doesn-39t-my-bot-see-messages-from-other-bots
         return False
-    if USER_AFKB and not (await event.get_sender()).bot:  # pylint:disable=E0602
-        if afkb_time:  # pylint:disable=E0602
+    if USER_AFK and not (await event.get_sender()).bot:  # pylint:disable=E0602
+        if afk_time:  # pylint:disable=E0602
             now = datetime.datetime.now()
-            datime_since_afkb = now - afkb_time  # pylint:disable=E0602
-            time = float(datime_since_afkb.seconds)
+            datime_since_afk = now - afk_time  # pylint:disable=E0602
+            time = float(datime_since_afk.seconds)
             days = time // (24 * 3600)
             time = time % (24 * 3600)
             hours = time // 3600
@@ -171,29 +171,29 @@ async def on_afkb(event):
             time %= 60
             seconds = time
             if days == 1:
-                afkb_since = "**Yesterday**"
+                afk_since = "**Yesterday**"
             elif days > 1:
                 if days > 6:
                     date = now + \
                         datetime.timedelta(
                             days=-days, hours=-hours, minutes=-minutes)
-                    afkb_since = date.strftime("%A, %Y %B %m, %H:%I")
+                    afk_since = date.strftime("%A, %Y %B %m, %H:%I")
                 else:
                     wday = now + datetime.timedelta(days=-days)
-                    afkb_since = wday.strftime('%A')
+                    afk_since = wday.strftime('%A')
             elif hours > 1:
-                afkb_since = f"`{int(hours)}h{int(minutes)}m` **ago**"
+                afk_since = f"`{int(hours)}h{int(minutes)}m` **ago**"
             elif minutes > 0:
-                afkb_since = f"`{int(minutes)}m{int(seconds)}s` **ago**"
+                afk_since = f"`{int(minutes)}m{int(seconds)}s` **ago**"
             else:
-                afkb_since = f"`{int(seconds)}s` **ago**"
+                afk_since = f"`{int(seconds)}s` **ago**"
         msg = None
-        message_to_reply = f"My Master {DEFAULTUSER} Is {reason} **Since** {total_afkb_time}" + \
+        message_to_reply = f"My Master {DEFAULTUSER} Is {reason} **Since** {total_afk_time}" + \
             f"\n__and HE may be back soon__\n**Just wait for my King's Reply" \
             if reason \
-            else f"My King 👑 {DEFAULTUSER} 👑 is **afk Since** {total_afkb_time}. \nand My King has left a word for you only: \n{AFKSK}\n`.` "
+            else f"My King 👑 {DEFAULTUSER} 👑 is **afk Since** {total_afk_time}. \nand My King has left a word for you only: \n{AFKSK}\n`.` "
         msg = await event.reply(message_to_reply)
         await asyncio.sleep(5)
-        if event.chat_id in last_afkb_message:  # pylint:disable=E0602
-            await last_afkb_message[event.chat_id].delete()  # pylint:disable=E0602
-        last_afkb_message[event.chat_id] = msg  # pylint:disable=E0602
+        if event.chat_id in last_afk_message:  # pylint:disable=E0602
+            await last_afk_message[event.chat_id].delete()  # pylint:disable=E0602
+        last_afk_message[event.chat_id] = msg  # pylint:disable=E0602
