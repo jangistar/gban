c68fd13ef303c20f71da3f5d56a2a2edd9142aed
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-04-30 07:01:15 +0530

    Update rapidleech.py

--- stdplugins/rapidleech.py
+++ stdplugins/rapidleech.py
@@ -7,7 +7,7 @@
 # you may use this file in compliance with the License.
 #
 
-"""Command : .rl link"""
+"""RapidLeech plugin: Inspired by @SjProjects"""
 
 import aiohttp
 import asyncio
@@ -22,7 +22,7 @@ logger.info(Config.OPEN_LOAD_LOGIN)
 # https://t.me/RoseSupport/33801
 
 
-@borg.on(admin_cmd("rl"))
+@borg.on(admin_cmd(pattern="rl"))
 async def _(event):
     if event.fwd_from:
         return
@@ -36,7 +36,7 @@ async def _(event):
             reply_message.message, reply_message.entities)
     converted_links = ""
     if len(list_of_urls) > 0:
-        converted_links += "Trying to generate IP specific link\n\nBhagwan se prathna karo.... \n"
+        converted_links += "Trying to generate IP specific link\n\nഞെക്കി പിടി \n"
         for a_url in list_of_urls:
             converted_link_infos = await get_direct_ip_specific_link(a_url)
             if "url" in converted_link_infos:
@@ -50,74 +50,12 @@ async def _(event):
 
 async def get_direct_ip_specific_link(link: str):
     # https://github.com/ytdl-org/youtube-dl/blob/master/youtube_dl/extractor/openload.py#L246-L255
-    OPEN_LOAD_DOMAINS = r"(?:openload\.(?:co|io|link|pw)|oload\.(?:tv|biz|stream|site|xyz|win|download|cloud|cc|icu|fun|club|info|press|pw|life|live|space|services|website)|oladblock\.(?:services|xyz|me)|openloed\.co)"
-    OPEN_LOAD_VALID_URL = r"(?x)https?://(?P<host>(?:www\.)?%s)/(?:f|embed)/(?P<id>[a-zA-Z0-9-_]+)" % OPEN_LOAD_DOMAINS
-    # https://github.com/ytdl-org/youtube-dl/blob/master/youtube_dl/extractor/openload.py#L246-L255
     # https://github.com/ytdl-org/youtube-dl/blob/master/youtube_dl/extractor/googledrive.py#L16-L27
     GOOGLE_DRIVE_VALID_URLS = r"(?x)https?://(?:(?:docs|drive)\.google\.com/(?:(?:uc|open)\?.*?id=|file/d/)|video\.google\.com/get_player\?.*?docid=)(?P<id>[a-zA-Z0-9_-]{28,})"
     # https://github.com/ytdl-org/youtube-dl/blob/master/youtube_dl/extractor/googledrive.py#L16-L27
     dl_url = None
-    if "zippyshare.com" in link:
-        async with aiohttp.ClientSession() as session:
-            http_response = await session.get(link)
-            http_response_text = await http_response.text()
-            response_b_soup = BeautifulSoup(http_response_text, "html.parser")
-            scripts = response_b_soup.find_all(
-                "script", {"type": "text/javascript"})
-            # calculations
-            # check https://github.com/LameLemon/ziggy/blob/master/ziggy.py
-            for script in scripts:
-                if "getElementById('dlbutton')" in script.text:
-                    regex_search_exp = re.search(
-                        '= (?P<url>\".+\" \+ (?P<math>\(.+\)) .+);', script.text)
-                    url_raw = regex_search_exp.group("url")
-                    math = regex_search_exp.group("math")
-                    dl_url = url_raw.replace(math, '"' + str(eval(math)) + '"')
-                    break
-            #
-            base_url = re.search("http.+.com", link).group()
-            dl_url = {
-                "url": base_url + eval(dl_url)
-            }
-    elif re.search(OPEN_LOAD_VALID_URL, link):
-        # https://stackoverflow.com/a/47726003/4723940
-        async with aiohttp.ClientSession() as session:
-            openload_id = re.search(OPEN_LOAD_VALID_URL, link).group("id")
-            step_one_url = "https://api.openload.co/1/file/dlticket?file={}&login={}&key={}".format(
-                openload_id, Config.OPEN_LOAD_LOGIN, Config.OPEN_LOAD_KEY)
-            http_response = await session.get(step_one_url)
-            http_response_text = await http_response.text()
-            http_response_json = json.loads(http_response_text)
-            logger.info(http_response_json)
-            if http_response_json["msg"] == "OK":
-                # wait till wait time
-                await asyncio.sleep(int(http_response_json["result"]["wait_time"]))
-                # TODO: check if captcha is required
-                step_two_url = "https://api.openload.co/1/file/dl?file={}&ticket={}".format(
-                    openload_id, http_response_json["result"]["ticket"])
-                http_response = await session.get(step_two_url)
-                http_response_text = await http_response.text()
-                http_response_json = json.loads(http_response_text)
-                logger.info(http_response_json)
-                if http_response_json["msg"] == "OK":
-                    dl_file_url = http_response_json["result"]["url"]
-                    dl_file_name = http_response_json["result"]["name"]
-                    dl_file_size = http_response_json["result"]["size"]
-                    dl_url = {
-                        "url": dl_file_url,
-                        "name": dl_file_name,
-                        "size": dl_file_size
-                    }
-                else:
-                    dl_url = {
-                        "err": http_response_text
-                    }
-            else:
-                dl_url = {
-                    "err": http_response_text
-                }
-        # https://stackoverflow.com/a/47726003/4723940
-    elif re.search(GOOGLE_DRIVE_VALID_URLS, link):
+    
+    if re.search(GOOGLE_DRIVE_VALID_URLS, link):
         file_id = re.search(GOOGLE_DRIVE_VALID_URLS, link).group("id")
         async with aiohttp.ClientSession(cookie_jar=aiohttp.CookieJar()) as session:
             step_zero_url = "https://drive.google.com/uc?export=download&id={}".format(file_id)
@@ -157,6 +95,6 @@ async def get_direct_ip_specific_link(link: str):
                     }
     else:
         dl_url = {
-            "err": "Unsupported URL"
+            "err": "Unsupported URL. Try @transload"
         }
     return dl_url
