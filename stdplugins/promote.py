8cf478715b40b6a4a818c7d9948861459893aabc
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-05-12 03:25:43 +0530

    Update promote.py

--- stdplugins/promote.py
+++ stdplugins/promote.py
@@ -1,4 +1,4 @@
-"""Reply to a user to .premote / .demote / .prankpremote them in the current chat"""
+"""Reply to a user to .premote / .demote  them in the current chat"""
 from telethon import events
 import asyncio
 from datetime import datetime
@@ -64,25 +64,4 @@ async def _(event):
         await event.edit("Successfully Demoted")
 
         
-@borg.on(admin_cmd(pattern="prankpremote ?(.*)"))
-async def _(event):
-    if event.fwd_from:
-        return
-    start = datetime.now()
-    to_prankpremote_id = None
-    rights = ChatAdminRights(
-        
-    )
-    input_str = event.pattern_match.group(1)
-    reply_msg_id = event.message.id
-    if reply_msg_id:
-        r_mesg = await event.get_reply_message()
-        to_prankpremote_id = r_mesg.sender_id
-    elif input_str:
-        to_prankpremote_id = input_str
-    try:
-        await borg(EditAdminRequest(event.chat_id, to_prankpremote_id, rights, ""))
-    except (Exception) as exc:
-        await event.edit(str(exc))
-    else:
-        await event.edit("admins has Successfully premoted Madanyu {to_prankpremote_id}")
+
