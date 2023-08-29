cbc0edd4d6f5f2d5ff4f5fa6efc48f5be2712435
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2023-08-29 18:53:20 +0530

    Create gban.sh

---
+++ gban.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+
+Install the necessary dependencies
+sudo apt install libleveldb-dev libffi-dev libssl-dev git-all python3 python3-pip
+
+Install Python 3.11
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.11
+
+
+git cloning the repo locally
+sudo git clone https://github.com/jangistar/gban
+
+cd gban
+
+sudo pip3 install -r requirements.txt
+
+sudo apt install nano
+
+sudo apt install python3
+
+sudo apt install python3-pip
+
+
+Install the Python packages
+sudo pip3 install --upgrade pip
+
+sudo apt install software-properties-common
+
+sudo add-apt-repository ppa:deadsnakes/ppa
+
+sudo apt install python3.11
+
+sudo apt-get update
+
+sudo apt update
+
+sudo apt-get upgrade
+
+sudo apt upgrade
+
+sudo pip install Telethon
+
+sudo pip install gitpython
+
+sudo pip install humanize
+
+sudo pip install emoji --upgrade
+
+sudo pip install googletrans
+
+sudo python3 -m pip install regex
+
+sudo pip install git+https://github.com/Joeclinton1/google-images-download.git
+
+sudo pip install wikipedia
+
+sudo pip install speedtest
+
+sudo pip install speedtest-cli
+
+sudo pip3 install --upgrade oauth2client
+
+sudo pip3 install --upgrade urbandict
+
+sudo pip3 install --upgrade asyncurban
+
+sudo pip3 install --upgrade qrcode
+
+sudo pip install google
+
+sudo pip install google-api-python-client
+
+sudo pip install --upgrade google-api-python-client
+
+sudo python3 -m pip install numpy
+
+sudo python3 -m pip install pycolor
+
+sudo python3 -m pip install psycopg
+
+sudo python3 -m pip install psycopg2-binary
+
+sudo python3 -m pip install aiohttp
+
+sudo python3 -m pip install hachoir
+
+sudo python3 -m pip install pySmartDL
+
+sudo pip install github
+
+sudo pip install pygithub
+
+sudo pip3 install selenium
+
+sudo pip3 install pytz
+
+sudo python3 -m pip install 'telegraph[aio]'
+
+sudo pip install beautifulsoup4
+
+sudo pip3 install httpx==0.19.0
+
+sudo pip3 install pylast
+
+sudo pip3 install markdown
+
+sudo pip3 uninstall httpcore
+
+sudo pip install httpcore[http2]
+
+sudo pip3 install -U https://github.com/LonamiWebs/Telethon/archive/master.zip
+
+sudo pip3 install redis
+
+sudo pip3 install httpx
+
+sudo python3 -m pip install python-dotenv
+
+sudo python3 -m pip install --upgrade pyfiglet
+
+sudo python3 -m pip install --upgrade googletrans
+
+sudo python3 -m pip install spamwatch
+
+sudo python3 -m pip install aiofiles
+
+sudo python3 -m pip install asyncio
+
+sudo python3 -m pip install pycolour
+
+sudo python3 -m pip install sqlalchemy
+
+sudo pip3 install pymongo
+
+sudo pip3 install PILLOW
+
+sudo pip3 install GOOGLE
+
+sudo python3 -m pip install --upgrade https://github.com/LonamiWebs/Telethon/archive/v1.zip
+
+deploying gban bot on local machine
+sudo python3 -m stdborg 5
